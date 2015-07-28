09e6e133b91d9996909280d8d3091a30f4a9e936
bkavuncu <bertankavuncu@gmail.com>
2015-07-28 21:46:01 +0100

    small fix

---
+++ GDO/Scripts/gdo.node.js
@@ -0,0 +1,22 @@
+﻿gdo.updateDisplayCanvas = function () {
+    /// <summary>
+    /// Updates the gdo canvas.
+    /// </summary>
+    /// <returns></returns>
+
+    if (gdo.clientMode == CLIENT_MODE.NODE) {
+        if (gdo.net.maintenanceMode) {
+            $("#maintenance_title").empty().css({ fontSize: 140 }).css("color", "#FFF").append("GDO Node <b>" + gdo.clientId + "</b>");
+            gdo.maintenance.drawEmptyNodeTable(gdo.net.cols, gdo.net.rows);
+            gdo.maintenance.drawMaintenanceTable();
+            $("#maintenance").show();
+        } else {
+            $("#maintenance").hide();
+        }
+
+    } else if (gdo.clientMode == CLIENT_MODE.CONTROL) {
+        // gdo.maintenance.drawNodeTable(gdo.nodeId);
+        gdo.maintenance.drawSectionTable();
+        gdo.maintenance.drawSectionButtonTable();
+    }
+}
