3eca1e87fb7e013ba7d3cbb34d78b9be9b1f8d16
bkavuncu <bkavuncu@users.noreply.github.com>
2015-08-12 23:14:47 +0300

    Update clean_apps.bat

--- clean_apps.bat
+++ clean_apps.bat
@@ -5,7 +5,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 for /D %%a in (GDO.Apps.*) do (
 	set str=%%a
     echo Removing !str:GDO.Apps.=!
-    del "%cd%\GDO\bin\%%a.*"
+    del "%cd%\GDO\bin\*.*"
 	echo y | rmdir "%cd%\GDO\Configurations\"!str:GDO.Apps.=! /S
 	echo y | rmdir "%cd%\GDO\Scripts\"!str:GDO.Apps.=! /S
 	echo y | rmdir "%cd%\GDO\Web\"!str:GDO.Apps.=! /S
