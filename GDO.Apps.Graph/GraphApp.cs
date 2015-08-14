95e0d61f40482815717d915eb727adcb68f0376c
David Chia <david.mhchia@gmail.com>
2015-08-14 22:47:29 +0100

    Half way through transcoding distributeGraph.js into GraphApp.cs. Also streamlined classes, combined 'From', 'To' classes to using 'Pos' class.

--- GDO.Apps.Graph/GraphApp.cs
+++ GDO.Apps.Graph/GraphApp.cs
@@ -37,7 +37,7 @@ namespace GDO.Apps.Graph
             public double height { get; set; }
         }
 
-        public class NodePos
+        public class Pos
         {
             public double x { get; set; }
             public double y { get; set; }
@@ -46,25 +46,14 @@ namespace GDO.Apps.Graph
         public class Node
         {
             public int id { get; set; }
-            public NodePos pos { get; set; }
+            public Pos pos { get; set; }
         }
 
-        public class From
-        {
-            public double x { get; set; }
-            public double y { get; set; }
-        }
-
-        public class To
-        {
-            public double x { get; set; }
-            public double y { get; set; }
-        }
 
         public class LinkPos
         {
-            public From from { get; set; }
-            public To to { get; set; }
+            public Pos from { get; set; }
+            public Pos to { get; set; }
         }
 
         public class Link
@@ -81,6 +70,7 @@ namespace GDO.Apps.Graph
             public List<Link> links { get; set; }
         }
 
+       
 
         // extra classes for individual browser's output
         public class BrowserPos
@@ -98,6 +88,11 @@ namespace GDO.Apps.Graph
 
         }
 
+        public class Scales
+        {
+            public double x { get; set; }
+            public double y { get; set; }
+        }
 
 
         // init is run when 'Deploy' is clicked
@@ -109,9 +104,12 @@ namespace GDO.Apps.Graph
             Directory.CreateDirectory(System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\graph");
         }
 
-        // Image is an abstract base class that provides functionality for the Bitmap and Metafile descended classes.
-
-
+        public int checkBrowserPos(Pos pos)
+        {
+            BrowserPos browserPos = new BrowserPos();
+            browserPos.row = Math.Floor(pos.y / Section.Height);
+            browserPos.col = Math.Floor(pos.x / Section.Width);
+        }
 
 
         public void ProcessGraph()   // add parameter string filename later, also may need to change return type
@@ -122,7 +120,7 @@ namespace GDO.Apps.Graph
             JsonSerializer serializer = new JsonSerializer();
             GraphCompleteData graphData = serializer.Deserialize<GraphCompleteData>(reader);
 
-            System.Diagnostics.Debug.WriteLine(graphData.rectDimension.height);
+            //System.Diagnostics.Debug.WriteLine(graphData.rectDimension.height);
 
 
             StreamWriter sw = new StreamWriter(System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\new_output.json");
@@ -130,90 +128,122 @@ namespace GDO.Apps.Graph
             JsonWriter writer = new JsonTextWriter(sw);
             serializer.Serialize(writer, graphData.nodes);
 
+            List<Node> nodes = graphData.nodes;
+            List<Link> links = graphData.links;
+            RectDimension rectDimension = graphData.rectDimension;
 
-            StreamWriter sw2 = new StreamWriter(System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\new_full_output.json");
-            sw2.AutoFlush = true;
-            JsonWriter writer2 = new JsonTextWriter(sw2);
-            serializer.Serialize(writer2, graphData);
+            // transform data to GDO dimension
+            Scales scales = new Scales();
+            scales.x = Section.Width / rectDimension.width;
+            scales.y = Section.Height / rectDimension.height;
 
-            /*
-                this.ImageName = imageName;
+            Double scaleDown = 0.9; // to make graph smaller and nearer to (0, 0)
 
-                String basePath = System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\images\\";
-                String path1 = basePath + ImageName;
-                Random imgDigitGenerator = new Random();
-                while (Directory.Exists(basePath + ImageNameDigit))
-                {
-                    this.ImageNameDigit = imgDigitGenerator.Next(10000, 99999).ToString();
-                }
-                String path2 = basePath + ImageNameDigit + "\\origin.png";
-                Directory.CreateDirectory(basePath + ImageNameDigit);
-                Image img1 = Image.FromFile(path1);
-                img1.Save(path2, ImageFormat.Png);
-                //File.Move(path1, path2);
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                nodes[i].pos.x *= scales.x * scaleDown;
+                nodes[i].pos.y *= scales.y * scaleDown;
 
+                // add padding on x, y to centralise graph after being scaled down
+                nodes[i].pos.x += Section.Width * (1 - scaleDown)/2;
+                nodes[i].pos.y += Section.Height * (1 - scaleDown) / 2;
+            }
 
-                //create origin
-                Image image = Image.FromFile(path2);
-                //image.Save(basePath + ImageNameDigit + "\\origin.png", ImageFormat.Png);
-                //File.Delete(path1);
+            for (int i = 0; i < links.Count; i++)
+            { 
+                links[i].pos.from.x *= scales.x * scaleDown;
+                links[i].pos.from.y *= scales.y * scaleDown;
+                links[i].pos.to.x *= scales.x * scaleDown;
+                links[i].pos.to.y *= scales.y * scaleDown;
 
-                //create thumnail
-                Image thumb = image.GetThumbnailImage(200 * image.Width / image.Height, 200, () => false, IntPtr.Zero);
-                thumb.Save(basePath + ImageNameDigit + "\\thumb.png", ImageFormat.Png);
-
-                double scaleWidth = (Section.Width + 0.000) / image.Width;
-                double scaleHeight = (Section.Height + 0.000) / image.Height;
-                int imageScaledWidth;
-                int imageScaledHeight;
-
-                int tempImageCols;
-                int tempImageRows;
+                links[i].pos.from.x += Section.Width * (1 - scaleDown) / 2;
+                links[i].pos.from.y += Section.Height * (1 - scaleDown) / 2;
+                links[i].pos.to.x += Section.Width * (1 - scaleDown) / 2;
+                links[i].pos.to.y += Section.Height * (1 - scaleDown) / 2;
+            }
 
 
-                if (scaleWidth > scaleHeight && mode == (int)Mode.FILL ||
-                    scaleWidth < scaleHeight && mode == (int)Mode.FIT)
-                {
-                    imageScaledWidth = (image.Width - 1) / Section.Cols + 1; // ceiling
-                    imageScaledHeight = (imageScaledWidth * (Section.Height / Section.Rows) - 1) / (Section.Width / Section.Cols) + 1;
-                    tempImageCols = Section.Cols;
-                    tempImageRows = Math.Max(Section.Rows, (image.Height - 1) / imageScaledHeight + 1); // ceiling
-                }
-                else
-                {
-                    imageScaledHeight = (image.Height - 1) / Section.Rows + 1; // ceiling
-                    imageScaledWidth = (imageScaledHeight * (Section.Width / Section.Cols) - 1) / (Section.Height / Section.Rows) + 1; // ceiling and keep ratio
-                    tempImageCols = Math.Max(Section.Cols, (image.Width - 1) / imageScaledWidth + 1); // ceiling
-                    tempImageRows = Section.Rows;
-                }
-                Tiles = new Image[tempImageCols, tempImageRows];
-                for (int i = 0; i < tempImageCols; i++)
-                {
-                    for (int j = 0; j < tempImageRows; j++)
+            GraphPartitionData[,] partitionData = new GraphPartitionData[Section.Row, Section.Col];
+
+
+                /*
+                    this.ImageName = imageName;
+
+                    String basePath = System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\images\\";
+                    String path1 = basePath + ImageName;
+                    Random imgDigitGenerator = new Random();
+                    while (Directory.Exists(basePath + ImageNameDigit))
                     {
-                        // each tile is assigned to a newly constructed Bitmap, with the size of each browser
-                        // Bitmap is a descendant of Image
-                        Tiles[i, j] = new Bitmap((Section.Width / Section.Cols), (Section.Height / Section.Rows));
-
-                        // assign Bitmap to graphics
-                        Graphics graphics = Graphics.FromImage(Tiles[i, j]);
-
-                        // then draw image using DrawImage which is part of systems.drawing
-                        // Draws the specified portion of the specified Image at the specified location and with the specified size.
-
-
-
-                        graphics.DrawImage(image,
-                            new Rectangle(0, 0, (Section.Width / Section.Cols), (Section.Height / Section.Rows)),
-                            new Rectangle(i * imageScaledWidth, j * imageScaledHeight, imageScaledWidth, imageScaledHeight),
-                            GraphicsUnit.Pixel);
-                        graphics.Dispose();
-                        path2 = basePath + ImageNameDigit + "\\" + "crop" + @"_" + i + @"_" + j + @".png";
-                        Tiles[i, j].Save(path2, ImageFormat.Png);
+                        this.ImageNameDigit = imgDigitGenerator.Next(10000, 99999).ToString();
                     }
-                }
-                return this.ImageNameDigit;
-             */
-        }
+                    String path2 = basePath + ImageNameDigit + "\\origin.png";
+                    Directory.CreateDirectory(basePath + ImageNameDigit);
+                    Image img1 = Image.FromFile(path1);
+                    img1.Save(path2, ImageFormat.Png);
+                    //File.Move(path1, path2);
+
+
+                    //create origin
+                    Image image = Image.FromFile(path2);
+                    //image.Save(basePath + ImageNameDigit + "\\origin.png", ImageFormat.Png);
+                    //File.Delete(path1);
+
+                    //create thumnail
+                    Image thumb = image.GetThumbnailImage(200 * image.Width / image.Height, 200, () => false, IntPtr.Zero);
+                    thumb.Save(basePath + ImageNameDigit + "\\thumb.png", ImageFormat.Png);
+
+                    double scaleWidth = (Section.Width + 0.000) / image.Width;
+                    double scaleHeight = (Section.Height + 0.000) / image.Height;
+                    int imageScaledWidth;
+                    int imageScaledHeight;
+
+                    int tempImageCols;
+                    int tempImageRows;
+
+
+                    if (scaleWidth > scaleHeight && mode == (int)Mode.FILL ||
+                        scaleWidth < scaleHeight && mode == (int)Mode.FIT)
+                    {
+                        imageScaledWidth = (image.Width - 1) / Section.Cols + 1; // ceiling
+                        imageScaledHeight = (imageScaledWidth * (Section.Height / Section.Rows) - 1) / (Section.Width / Section.Cols) + 1;
+                        tempImageCols = Section.Cols;
+                        tempImageRows = Math.Max(Section.Rows, (image.Height - 1) / imageScaledHeight + 1); // ceiling
+                    }
+                    else
+                    {
+                        imageScaledHeight = (image.Height - 1) / Section.Rows + 1; // ceiling
+                        imageScaledWidth = (imageScaledHeight * (Section.Width / Section.Cols) - 1) / (Section.Height / Section.Rows) + 1; // ceiling and keep ratio
+                        tempImageCols = Math.Max(Section.Cols, (image.Width - 1) / imageScaledWidth + 1); // ceiling
+                        tempImageRows = Section.Rows;
+                    }
+                    Tiles = new Image[tempImageCols, tempImageRows];
+                    for (int i = 0; i < tempImageCols; i++)
+                    {
+                        for (int j = 0; j < tempImageRows; j++)
+                        {
+                            // each tile is assigned to a newly constructed Bitmap, with the size of each browser
+                            // Bitmap is a descendant of Image
+                            Tiles[i, j] = new Bitmap((Section.Width / Section.Cols), (Section.Height / Section.Rows));
+
+                            // assign Bitmap to graphics
+                            Graphics graphics = Graphics.FromImage(Tiles[i, j]);
+
+                            // then draw image using DrawImage which is part of systems.drawing
+                            // Draws the specified portion of the specified Image at the specified location and with the specified size.
+
+
+
+                            graphics.DrawImage(image,
+                                new Rectangle(0, 0, (Section.Width / Section.Cols), (Section.Height / Section.Rows)),
+                                new Rectangle(i * imageScaledWidth, j * imageScaledHeight, imageScaledWidth, imageScaledHeight),
+                                GraphicsUnit.Pixel);
+                            graphics.Dispose();
+                            path2 = basePath + ImageNameDigit + "\\" + "crop" + @"_" + i + @"_" + j + @".png";
+                            Tiles[i, j].Save(path2, ImageFormat.Png);
+                        }
+                    }
+                    return this.ImageNameDigit;
+                 */
+            }
     }
 }
