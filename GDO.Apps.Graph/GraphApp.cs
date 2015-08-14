4322c51b1befcc15879d47920f3653decac18aaf
David Chia <david.mhchia@gmail.com>
2015-08-14 11:16:21 +0100

    Bug in Control.cshtml. Shows error initControl() and RequestRendering() is not a function.

--- GDO.Apps.Graph/GraphApp.cs
+++ GDO.Apps.Graph/GraphApp.cs
@@ -1,11 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
+using GDO.Core;
+using GDO.Utility;
 
 namespace GDO.Apps.Graph
 {
-    public class GraphApp
+    enum Mode
     {
+        FILL = 1,
+        FIT = 0
+    };
+    public class GraphApp : IAppInstance
+    {
+        public int Id { get; set; }
+        public Section Section { get; set; }
+        public AppConfiguration Configuration { get; set; }
+        public string ImageName { get; set; }
+        public string ImageNameDigit { get; set; }
+        public int DisplayMode { get; set; }
+        public Image[,] Tiles { get; set; }
+        public void init(int instanceId, Section section, AppConfiguration configuration)
+        {
+            this.Id = instanceId;
+            this.Section = section;
+            this.Configuration = configuration;
+            this.DisplayMode = (int)Mode.FIT;
+            Directory.CreateDirectory(System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\images");
+        }
+
+        // Image is an abstract base class that provides functionality for the Bitmap and Metafile descended classes.
+
+        public string ProcessImage(string imageName, int mode)
+        {
+            this.ImageName = imageName;
+
+            String basePath = System.Web.HttpContext.Current.Server.MapPath("~/") + @"\Web\Graph\images\\";
+            String path1 = basePath + ImageName;
+            Random imgDigitGenerator = new Random();
+            while (Directory.Exists(basePath + ImageNameDigit))
+            {
+                this.ImageNameDigit = imgDigitGenerator.Next(10000, 99999).ToString();
+            }
+            String path2 = basePath + ImageNameDigit + "\\origin.png";
+            Directory.CreateDirectory(basePath + ImageNameDigit);
+            Image img1 = Image.FromFile(path1);
+            img1.Save(path2, ImageFormat.Png);
+            //File.Move(path1, path2);
+
+        
+            //create origin
+            Image image = Image.FromFile(path2);
+            //image.Save(basePath + ImageNameDigit + "\\origin.png", ImageFormat.Png);
+            //File.Delete(path1);
+
+            //create thumnail
+            Image thumb = image.GetThumbnailImage(200 * image.Width / image.Height, 200, () => false, IntPtr.Zero);
+            thumb.Save(basePath + ImageNameDigit + "\\thumb.png", ImageFormat.Png);
+
+            double scaleWidth = (Section.Width + 0.000) / image.Width;
+            double scaleHeight = (Section.Height + 0.000) / image.Height;
+            int imageScaledWidth;
+            int imageScaledHeight;
+
+            int tempImageCols;
+            int tempImageRows;
+
+
+            if (scaleWidth > scaleHeight && mode == (int)Mode.FILL ||
+                scaleWidth < scaleHeight && mode == (int)Mode.FIT)
+            {
+                imageScaledWidth = (image.Width - 1) / Section.Cols + 1; // ceiling
+                imageScaledHeight = (imageScaledWidth * (Section.Height / Section.Rows) - 1) / (Section.Width / Section.Cols) + 1;
+                tempImageCols = Section.Cols;
+                tempImageRows = Math.Max(Section.Rows, (image.Height - 1) / imageScaledHeight + 1); // ceiling
+            }
+            else
+            {
+                imageScaledHeight = (image.Height - 1) / Section.Rows + 1; // ceiling
+                imageScaledWidth = (imageScaledHeight * (Section.Width / Section.Cols) - 1) / (Section.Height / Section.Rows) + 1; // ceiling and keep ratio
+                tempImageCols = Math.Max(Section.Cols, (image.Width - 1) / imageScaledWidth + 1); // ceiling
+                tempImageRows = Section.Rows;
+            }
+            Tiles = new Image[tempImageCols, tempImageRows];
+            for (int i = 0; i < tempImageCols; i++)
+            {
+                for (int j = 0; j < tempImageRows; j++)
+                {
+                    // each tile is assigned to a newly constructed Bitmap, with the size of each browser
+                    // Bitmap is a descendant of Image
+                    Tiles[i, j] = new Bitmap((Section.Width / Section.Cols), (Section.Height / Section.Rows));
+                    
+                    // assign Bitmap to graphics
+                    Graphics graphics = Graphics.FromImage(Tiles[i, j]);
+
+                    // then draw image using DrawImage which is part of systems.drawing
+                    // Draws the specified portion of the specified Image at the specified location and with the specified size.
+      
+                    /*
+                    public void DrawImage(
+	                    Image image,
+	                    RectangleF destRect,   // this is the coordinates of rect in tile's Bitmap
+	                    RectangleF srcRect,    // here it uses i & j to get the right portion of scaled image
+	                    GraphicsUnit srcUnit
+                    )
+                    */
+       
+                    graphics.DrawImage(image,
+                        new Rectangle(0, 0, (Section.Width / Section.Cols), (Section.Height / Section.Rows)),
+                        new Rectangle(i * imageScaledWidth, j * imageScaledHeight, imageScaledWidth, imageScaledHeight),
+                        GraphicsUnit.Pixel);
+                    graphics.Dispose();
+                    path2 = basePath + ImageNameDigit + "\\" + "crop" + @"_" + i + @"_" + j + @".png";
+                    Tiles[i, j].Save(path2, ImageFormat.Png);
+                }
+            }
+            return this.ImageNameDigit;
+        }
     }
 }
--- GDO.Apps.Graph/GraphAppHub.cs
+++ GDO.Apps.Graph/GraphAppHub.cs
@@ -1,11 +1,112 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Registration;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using GDO.Core;
+using GDO.Utility;
 
 namespace GDO.Apps.Graph
 {
-    public class GraphAppHub
+    [Export(typeof(IAppHub))]
+    public class GraphAppHub : Hub, IAppHub
     {
+        public string Name { get; set; } = "Graph";
+        public int P2PMode { get; set; } = (int)Cave.P2PModes.Neighbours;
+        public Type InstanceType { get; set; } = new GraphApp().GetType();
+        public void JoinGroup(int instanceId)
+        {
+            Groups.Add(Context.ConnectionId, "" + instanceId);
+        }
+        public void ExitGroup(int instanceId)
+        {
+            Groups.Remove(Context.ConnectionId, "" + instanceId);
+        }
+
+        public void RequestRendering (int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {    // create the GraphApp object later on
+                    //GraphApp ga = (ImagesApp)Cave.Apps["Graph"].Instances[instanceId];
+                    //string imageNameDigit = ia.ProcessImage(imageName, ia.DisplayMode);
+                    try // the 'try' 'catch' here seems redundant
+                    {   // Clients.Group to let all clients within the group receive image name
+                        Clients.Group("" + instanceId).RenderGraph(); // pass in parameters later
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        /*
+
+        // this function is used to initialise ImagesApp object and imageNameDigit (obtained thru processImage function)
+        public void ChangeImageName(int instanceId, string imageName)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    ImagesApp ia = (ImagesApp)Cave.Apps["Images"].Instances[instanceId];
+                    string imageNameDigit = ia.ProcessImage(imageName, ia.DisplayMode);
+                    SendImageNames(instanceId, imageName, imageNameDigit);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        // called by ChangeImageName above to let all clients receive image name (when new photo is uploaded after nodes have been initiated probably)
+        public void SendImageNames(int instanceId, string imageName, string imageNameDigit)
+        {
+            try
+            {   // Clients.Group to let all clients within the group receive image name
+                Clients.Group("" + instanceId).receiveImageName(imageName, imageNameDigit);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        // called by clients (App or Control) to get image name from server, which will in turn call Client's receiveImageName function that will render the image
+        public void RequestImageName(int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    if (((ImagesApp)Cave.Apps["Images"].Instances[instanceId]).ImageName != null)
+                    {
+                        Clients.Caller.receiveImageName(((ImagesApp)Cave.Apps["Images"].Instances[instanceId]).ImageName,
+                                                        ((ImagesApp)Cave.Apps["Images"].Instances[instanceId]).ImageNameDigit);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        */
     }
 }
