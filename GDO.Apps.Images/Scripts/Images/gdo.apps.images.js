db336dcecdef4d51324b0ce2dbcd2d5679fbe3b4
unknown <zhangjqsmiling@gmail.com>
2015-08-20 11:31:23 +0100

    images app support status store

--- GDO.Apps.Images/Scripts/Images/gdo.apps.images.js
+++ GDO.Apps.Images/Scripts/Images/gdo.apps.images.js
@@ -3,16 +3,19 @@
     $.connection.imagesAppHub.client.receiveImageName = function (imageName, imageNameDigit) {
         if (gdo.clientMode == gdo.CLIENT_MODE.CONTROL) {
             gdo.consoleOut('.IMAGETILES', 1, 'Instance - ' + gdo.controlId + ": Downloading Image : " + imageName + " with id: " + imageNameDigit);
-            $("iframe").contents().find("#thumbnail_image").attr("src", "\\Web\\Images\\images\\" + imageNameDigit + "\\thumb.png");
+            $("iframe").contents().find("#thumbnail_control　> img").attr("src", "\\Web\\Images\\images\\" + imageNameDigit + "\\thumb.png");
+            $("iframe")[0].contentWindow.initializeCropper();
         } else if (gdo.clientMode == gdo.CLIENT_MODE.NODE) {
             gdo.consoleOut('.IMAGETILES', 1, 'Instance - ' + gdo.clientId + ": Downloading Image : " + imageName + "_" + gdo.net.node[gdo.clientId].appInstanceId + "_" + gdo.net.node[gdo.clientId].sectionCol + "_" + gdo.net.node[gdo.clientId].sectionRow + ".png");
             $("iframe").contents().find("#image_tile").attr("src", "\\Web\\Images\\images\\" + imageNameDigit + "\\" + "crop" + "_" + gdo.net.node[gdo.clientId].sectionCol + "_" + gdo.net.node[gdo.clientId].sectionRow + ".png");
         }
     }
+    /*
     $.connection.imagesAppHub.client.receiveDisplayMode = function(display_mode) {
         gdo.net.app["Images"].display_mode = display_mode;
         gdo.net.app["Images"].setDisplayModeSelect();
     }
+    */
     $.connection.imagesAppHub.client.setMessage = function (message) {
         if (gdo.clientMode == gdo.CLIENT_MODE.CONTROL) {
             gdo.consoleOut('.Images', 1, 'Message from server:' + message);
@@ -28,7 +31,7 @@
                 var imageJson = JSON.parse(imageInfo);
                 $("iframe")[0].contentWindow.setThumbNailImageInfo(imageJson);
             } else {
-                gdo.consoleOut('.Images', 1, 'Thumbnail image is null');
+                $("iframe")[0].contentWindow.setThumbNailDisplayMode(1); // FILL as Default
             }
         } else if (gdo.clientMode == gdo.CLIENT_MODE.NODE) {
             //do nothing
@@ -44,16 +47,19 @@
     }
 });
 
-gdo.net.app["Images"].display_mode = 0;
+//gdo.net.app["Images"].display_mode = 0;
 gdo.net.app["Images"].control_status = 0; // 0 disabled, 1 activated
 
-gdo.net.app["Images"].setDisplayModeSelect = function() {
+/*
+gdo.net.app["Images"].setDisplayModeSelect = function () {
     if (gdo.net.app["Images"].display_mode == 0) {
         $("iframe").contents().find("#display_mode_value").html("FIT Mode");
     } else {
         $("iframe").contents().find("#display_mode_value").html("FILL Mode");
     }
+    $("iframe")[0].contentWindow.setThumbNailDisplayMode();
 }
+*/
 
 gdo.net.app["Images"].initClient = function () {
     gdo.consoleOut('.IMAGETILES', 1, 'Initializing Image Tiles App Client at Node ' + gdo.clientId);
@@ -63,10 +69,10 @@ gdo.net.app["Images"].initClient = function () {
 gdo.net.app["Images"].initControl = function () {
     gdo.controlId = getUrlVar("controlId");
     gdo.net.app["Images"].control_status = 0;
-    gdo.net.app["Images"].server.requestDisplayMode(gdo.controlId);
+    //gdo.net.app["Images"].server.requestDisplayMode(gdo.controlId);
     gdo.net.app["Images"].server.requestImageName(gdo.controlId);
     gdo.net.app["Images"].server.requestSectionSize(gdo.controlId);
-    //gdo.net.app["Images"].server.requestThumNailImageInfo(gdo.controlId);
+    gdo.net.app["Images"].server.requestThumNailImageInfo(gdo.controlId);
     gdo.consoleOut('.IMAGETILES', 1, 'Initializing Image Tiles App Control at Instance ' + gdo.controlId);
 }
 
