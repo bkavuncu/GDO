2e84ef38608624833ab80e6bdd4315943c79aba9
bkavuncu <bertankavuncu@gmail.com>
2015-09-16 16:13:32 +0100

    mapsapp 2.0 add modify source functions

---
+++ GDO.Apps.Maps/Core/Sources/Tiles/ImageTileSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GDO.Apps.Maps.Core.Sources.Tiles
+{
+    public class ImageTileSource : TileSource
+    {
+        public bool Opaque { get; set; }
+        //TODO find representaion for functions in this class
+
+        new public void Modify(bool opaque)
+        {
+            Opaque = opaque;
+        }
+    }
+}
---
+++ GDO.Apps.Maps/Core/Sources/Tiles/JSONTileSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GDO.Apps.Maps.Core.Sources.Tiles
+{
+    public class JSONTileSource : TileSource
+    {
+        public string Url { get; set; }
+
+        new public void Modify(string url)
+        {
+            Url = url;
+        }
+    }
+}
