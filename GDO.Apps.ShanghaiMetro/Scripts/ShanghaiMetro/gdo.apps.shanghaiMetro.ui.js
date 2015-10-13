31a1811c0624ce65738e3ba9beee72042f6828e8
bkavuncu <bertankavuncu@gmail.com>
2015-10-13 13:11:16 +0100

    shangai metro app improvements

--- GDO.Apps.ShanghaiMetro/Scripts/ShanghaiMetro/gdo.apps.shanghaiMetro.ui.js
+++ GDO.Apps.ShanghaiMetro/Scripts/ShanghaiMetro/gdo.apps.shanghaiMetro.ui.js
@@ -26,7 +26,7 @@ gdo.net.app["ShanghaiMetro"].drawMapTable = function (instanceId) {
         .click(function () {
             gdo.net.app["ShanghaiMetro"].server.setBingLayerVisible(instanceId);
         });
-    $("iframe").contents().find("#map_table_row_0_col_1")
+    /*$("iframe").contents().find("#map_table_row_0_col_1")
         .empty()
         .append("<div><button type='button' id='stamen_button' class='btn btn-danger btn-block'>Stamen Maps</button></div>")
         .css("margin", "0px")
@@ -36,7 +36,7 @@ gdo.net.app["ShanghaiMetro"].drawMapTable = function (instanceId) {
         .unbind()
         .click(function () {
             gdo.net.app["ShanghaiMetro"].server.setStamenLayerVisible(instanceId);
-        });
+        });*/
     $("iframe").contents().find("#map_table_row_0_col_2")
         .empty()
         .append("<div><button type='button' id='stations_button' class='btn btn-danger btn-block'>Stations</button></div>")
