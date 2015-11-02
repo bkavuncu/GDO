1dc61cc3871d25d82918dd4abc5146c538d47603
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-11-02 13:37:27 +0000

    Almost Working

---
+++ GDO.Apps.Hercules/HerculesApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using GDO.Core;
+using Newtonsoft.Json;
+
+namespace GDO.Apps.Hercules
+{
+    enum Mode
+    {
+        CROP=1,
+        FIT=0
+    };
+    public class HerculesApp : IAppInstance
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public Section Section { get; set; }
+        public AppConfiguration Configuration { get; set; }
+
+        public void init(int instanceId, string appName, Section section, AppConfiguration configuration)
+        {
+            System.Diagnostics.Debug.Write("YOOOO SUPPPPP");
+            this.Id = instanceId;
+            this.AppName = appName;
+            this.Section = section;
+            this.Configuration = configuration;
+        }
+    }
+}
---
+++ GDO.Apps.Hercules/HerculesAppHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Registration;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using GDO.Core;
+using GDO.Utility;
+using Newtonsoft.Json;
+
+//[assembly: System.Web.UI.WebResource("GDO.Apps.Youtube.Scripts.Youtube.js", "application/x-javascript")]
+//[assembly: System.Web.UI.WebResource("GDO.Apps.Youtube.Configurations.sample.js", "application/json")]
+
+namespace GDO.Apps.Hercules
+{
+    [Export(typeof(IAppHub))]
+    public class HerculesAppHub : Hub, IAppHub
+    {
+        public string Name { get; set; } = "Hercules";
+        public int P2PMode { get; set; } = (int)Cave.P2PModes.Neighbours;
+        public Type InstanceType { get; set; } = new HerculesApp().GetType();
+        public void JoinGroup(int instanceId)
+        {
+            Groups.Add(Context.ConnectionId, "" + instanceId);
+        }
+        public void ExitGroup(int instanceId)
+        {
+            Groups.Remove(Context.ConnectionId, "" + instanceId);
+        }
+    }
+}
