1ce803498f3f02aabcfcec6ed4d4c1a29fe5e9be
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-11-10 20:49:17 +0000

    fixed a bug where the nodes would display on more than 4 rows

--- GDO.Apps.Hercules/Scripts/src/DeployerGrid.jsx
+++ GDO.Apps.Hercules/Scripts/src/DeployerGrid.jsx
@@ -1,34 +1,35 @@
 const React = require('react'),
-	View = require('./ui/View.jsx'),
-	ReactDOM = require('react-dom'),
-	_ = require('underscore');
+    View = require('./ui/View.jsx'),
+    ReactDOM = require('react-dom'),
+    _ = require('underscore');
 
-
+const NODE_PADDING = 3;
 class DeployerNode extends React.Component {
     render () {
-        var edge = Math.min(this.props.width, this.props.height);
-            
+        var edge = Math.min(this.props.width, this.props.height) - 2 * NODE_PADDING;
+
         var outerStyle = {
-            display: "flex",
-            justifyContent: "center",
-            width: edge + 'px',
-            height: edge + 'px',
-            padding: '5px'
-        },
-        	innerStyle = {
-        		display: 'flex',
-        		flexDirection: 'column',
-        		justifyContent: 'center',
-        		alignItems: 'center',
-        		backgroundColor: 'gainsboro',
-        		boxShadow: '0 0 3px gray',
-        		color: 'white',
-        		fontSize: '30px'
-        	};
+                display: "flex",
+                justifyContent: "center",
+                width: edge + 'px',
+                height: edge + 'px',
+                padding: NODE_PADDING + 'px'
+            },
+            innerStyle = {
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'darkgray',
+                boxShadow: '0 0 3px black',
+                color: 'white',
+                fontSize: '30px',
+                borderRadius: '0px'
+            };
         return <View id={'node' + this.props.id } className="node" style={outerStyle}>
             <View style={innerStyle}>
-            	{this.props.id}
-            	</View>
+                {this.props.id}
+            </View>
         </View>;
     }
 }
@@ -36,20 +37,20 @@ class DeployerNode extends React.Component {
 
 let [MEASURE, RENDER] = [0,1];
 class DeployerGrid extends React.Component {
-    	constructor (props) {
+    constructor (props) {
         super(props);
         this.state = {
             step: MEASURE
         };
     }
     componentDidMount () {
-    	var listener = () => this.resize();
-    	this.setState({listener});
+        var listener = () => this.resize();
+        this.setState({listener});
         window.addEventListener('resize', listener);
         this.resize();
     }
     componentWillUnmount () {
-    	console.log('yoooo');
+        console.log('yoooo');
         window.removeEventListener('resize', this.state.listener);
     }
     resize () {
@@ -63,20 +64,31 @@ class DeployerGrid extends React.Component {
     }
     render() {
         const NODE_NUMBER = 64,
-            ROWS = 4, 
-            COLUMNS = NODE_NUMBER / ROWS;
+            ROWS = 4,
+            COLUMNS = NODE_NUMBER / ROWS,
+            PADDING = 10;
+
+        var style = {
+            alignContent: 'flex-start',
+            padding: PADDING + 'px'
+        };
 
         switch (this.state.step) {
             case MEASURE:
                 return <View/>;
                 break;
             case RENDER:
-                var nW = this.state.width / COLUMNS,
-                    nH = this.state.height / ROWS;
+                var width = this.state.width - 2 * PADDING,
+                    height = this.state.height - 2 * PADDING,
+                    nW = width / COLUMNS,
+                    nH = height / ROWS;
 
-                var nodeList = _.range(1, NODE_NUMBER + 1).map((i) => <DeployerNode key={i} id={i} width={nW} height={nH} />);
+                var nodeList = _.range(1, NODE_NUMBER + 1)
+                    .map((i) => <DeployerNode
+                        key={i} id={i}
+                        width={nW} height={nH} />);
 
-                return <View>{nodeList}</View>;
+                return <View style={style}>{nodeList}</View>;
                 break;
         }
     }
