55b49eaba433cf716cc41b4d5786509ff56ecb01
Lorenzo Calegari <lmc13@imperial.ac.uk>
2015-11-17 11:34:25 +0000

    Added JsSchema and ServerDS

---
+++ GDO.Apps.Hercules/BackEnd/DB/JsSchema.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.src.Augment
+{
+
+
+    class JsStats
+    {
+        public double min;
+        public double max;
+        public double mean;
+        public double median;
+        public double variance;
+        public double stdDev;
+        public bool isEnum;
+    }
+
+
+    class JsField
+    {
+        public string name;
+        public string description;
+        public string type;
+        public string origin;
+        public bool disabled;
+        public JsStats stats;
+    }
+
+
+    class JsMiniset
+    {
+        public string name;
+        public string description;
+        public int id;
+        public int nrows;
+        public string sourceType;
+        public string sourceOrigin;
+        public bool disabled;
+        public JsField[] fields;
+    }
+
+
+    class JsDataset
+    {
+        public JsMiniset schema;
+        public dynamic[][] rows;
+    }
+
+
+}
---
+++ GDO.Apps.Hercules/BackEnd/DB/ServerDS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.src.DB
+{
+    public sealed class ServerDS
+    {
+        public static void GellAllMinisets() { }
+
+
+        public static void GetDataset(int id) { }
+
+
+        // Returns ID
+        public static int ParseDSFromFile(string path)
+        {
+
+        }
+
+    }
+}
