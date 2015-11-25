e6406422923ba844f08e6501b2ccc071a593e84c
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-11-25 20:08:14 +0000

    Major work on DataFilter, pretty list is pretty

--- GDO.Apps.Hercules/Scripts/src/DataFilter.js
+++ GDO.Apps.Hercules/Scripts/src/DataFilter.js
@@ -1,33 +1,96 @@
-const React = require('react');
+const React = require('react'),
+    DatasetStore = require('stores/DatasetStore');
 
 
-const fieldStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexBasis: '50px'
-}, Field = (field) => {
+const Field = (props) => {
+    var zIndex = props.listLength - props.index,
+        fieldStyle = {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexBasis: '30px',
+        padding: '10px',
+        color: 'white',
+        boxShadow: '0 0 3px black',
+        zIndex: zIndex,
+        backgroundColor: props.backgroundColor
+    };
+
     return (
         <div style={fieldStyle}>
-            {field.name}
+            {props.name}
         </div>
     );
 };
 
-class FieldList extends React.Component () {
+class FilterPanel extends React.Component {
+    static _getContainerStyle () {
+        return {
+            flex: '1 1 auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch'
+        };
+    }
+
     render () {
-        return <div style={style}>
-            {this.props.fields.map(Field)}
+        return <div id="filter-panel" style={FilterPanel._getContainerStyle()}>
+        </div>;
+    }
+}
+
+const colors = ['#7986CB', '#5C6BC0', '#3F51B5', '#3949AB', '#303F9F', '#283593', '#1A237E'],
+    pickColor = (index) => {
+        var l = colors.length - 1,
+            evenCycle = (Math.floor(index / l) % 2) === 0,
+            selector = (index % l) + 1;
+
+        if (evenCycle)
+            return colors[selector];
+        else
+            return colors[l - selector];
+    };
+
+class FieldList extends React.Component {
+    static _getContainerStyle () {
+        return {
+            flex: '0 0 200px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+            boxShadow: '0 0 5px gray',
+            backgroundColor: '#607D8B'
+        };
+    }
+
+    render () {
+        var {fields} = this.props;
+
+        return <div style={FieldList._getContainerStyle()}>
+            {fields.map(
+                (f, i) =>
+                    <Field {...f} key={i}
+                                  listLength={fields.length}
+                                  index={i}
+                                  backgroundColor={pickColor(i)}/>)}
         </div>;
     }
 }
 
 class DataFilter extends React.Component {
+    static _getContainerStyle () {
+        return {
+            display: 'flex',
+            alignItems: 'stretch'
+        };
+    }
 
     render () {
+        var {miniset} = this.props;
+
         return (
-            <div style={outerStyle}>
-                <FieldList />
+            <div style={DataFilter._getContainerStyle()}>
+                <FieldList fields={miniset.fields}/>
                 <FilterPanel />
             </div>
         );
@@ -35,11 +98,39 @@ class DataFilter extends React.Component {
 }
 
 
-export default class DataFilterWrapper extends React.Component {
-    componentWillMount () {
+class DataFilterWrapper extends React.Component {
+    constructor (props) {
+        super (props);
 
+        this.state = {
+            datasetId: DatasetStore.getActiveDatasetId()
+        };
     }
+    componentWillMount () {
+        var listener = this._onChange.bind(this);
+
+        DatasetStore.addChangeListener(listener);
+
+        this.setState({listener});
+    }
+
+    componentWillUnmount () {
+        DatasetStore.removeChangeListener(this.state.listener);
+    }
+
+    _onChange () {
+        this.setState({
+            datasetId: DatasetStore.getActiveDatasetId()
+        });
+    }
+
     render () {
-        return <DataFilter fields={this.state.fields} />;
+        var miniset = DatasetStore.getMiniSet(this.state.datasetId);
+
+        return <DataFilter miniset={miniset} />;
     }
 }
+
+DataFilterWrapper.prototype.tabName = 'Data Filter';
+
+module.exports = DataFilterWrapper;
