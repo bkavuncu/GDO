aa9e2c8475b220aa7262ba2d339c951ac89684ae
davidbirchwork <davidbirchwork@users.noreply.github.com>
2015-11-26 18:35:28 +0000

    Update ShanghaiMetroApp.cs
    slowing down time

--- GDO.Apps.ShanghaiMetro/ShanghaiMetroApp.cs
+++ GDO.Apps.ShanghaiMetro/ShanghaiMetroApp.cs
@@ -24,7 +24,7 @@ namespace GDO.Apps.ShanghaiMetro
         public bool HeatmapLayer { get; set; }
         public int TimeStep { get; set; } = 0;
         public bool IsAnimating { get; set; } = false;
-        public int WaitTime { get; set; } = 70;
+        public int WaitTime { get; set; } = 140;
 
         public float Opacity { get; set; }
         public int Blur { get; set; }
