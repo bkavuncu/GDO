082f6b28062baeb104584aa732cef0bff417b6c4
Dan Thomas <Dan Thomas>
2015-12-01 19:39:34 +0000

    New project setup from template

---
+++ GDO.Apps.Fractals/FractalsAppHub.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using GDO.Core;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json.Linq;
+
+namespace GDO.Apps.Fractals
+{
+    [Export(typeof(IAppHub))]
+    public class FractalsAppHub : Hub, IAppHub
+    {
+        public string Name { get; set; } = "Fractals";
+        public int P2PMode { get; set; } = (int)Cave.P2PModes.None;
+        public Type InstanceType { get; set; } = new FractalsApp().GetType();
+
+        public void JoinGroup(int instanceId)
+        {
+            Groups.Add(Context.ConnectionId, "" + instanceId);
+        }
+
+        public void ExitGroup(int instanceId)
+        {
+            Groups.Remove(Context.ConnectionId, "" + instanceId);
+        }
+
+        public void SetName(int instanceId, string name)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    ((FractalsApp)Cave.Apps["Fractals"].Instances[instanceId]).SetName(name);
+                    Clients.Group("" + instanceId).receiveName(instanceId, name);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void RequestName(int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    Clients.Caller.receiveName(instanceId, ((FractalsApp)Cave.Apps["Fractals"].Instances[instanceId]).GetName());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
---
+++ GDO.Apps.Fractals/Scripts/Fractals/gdo.apps.Fractals.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    gdo.consoleOut('.Fractals', 1, 'Loaded Fractals JS');
+    $.connection.fractalsAppHub.client.receiveName = function (instanceId, name) {
+        if (gdo.clientMode == gdo.CLIENT_MODE.CONTROL) {
+            gdo.consoleOut('.Fractals', 1, 'Instance - ' + instanceId + ": Received Name : " + name);
+        } else if (gdo.clientMode == gdo.CLIENT_MODE.NODE) {
+            gdo.consoleOut('.Fractals', 1, 'Instance - ' + instanceId + ": Received Name : " + name);
+            $("iframe").contents().find("#hello_text").empty().append("Hello " + name);
+        }
+    }
+});
+
+gdo.net.app["Fractals"].initClient = function () {
+    gdo.consoleOut('.Fractals', 1, 'Initializing Fractals App Client at Node ' + gdo.clientId);
+}
+
+gdo.net.app["Fractals"].initControl = function () {
+    gdo.controlId = getUrlVar("controlId");
+    gdo.net.app["Fractals"].server.requestName(gdo.controlId);
+    gdo.consoleOut('.Fractals', 1, 'Initializing Fractals App Control at Instance ' + gdo.controlId);
+
+    $("iframe").contents().find("#hello_submit")
+    .unbind()
+    .click(function () {
+        gdo.consoleOut('.Fractals', 1, 'Sending Name to Clients :' + $("iframe").contents().find('#hello_input').val());
+        gdo.net.app["Fractals"].server.setName(gdo.controlId, $("iframe").contents().find('#hello_input').val());
+    });
+}
+
+gdo.net.app["Fractals"].terminateClient = function () {
+    gdo.consoleOut('.Fractals', 1, 'Terminating Fractals App Client at Node ' + clientId);
+}
+
+gdo.net.app["Fractals"].ternminateControl = function () {
+    gdo.consoleOut('.Fractals', 1, 'Terminating Fractals App Control at Instance ' + gdo.controlId);
+}
