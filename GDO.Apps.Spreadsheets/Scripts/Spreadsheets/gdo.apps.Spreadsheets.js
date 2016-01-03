41c4128fe2bc179564881caa4b173b4240010910
Siddharth <siddharth_vasu@hotmail.co.uk>
2016-01-03 16:16:14 +0000

    Added Spreadsheet app and changed web.config to load to management page instead of IIS error.

---
+++ GDO.Apps.Spreadsheets/Scripts/Spreadsheets/gdo.apps.Spreadsheets.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    gdo.consoleOut('.Spreadsheets', 1, 'Loaded Spreadsheets JS');
+    $.connection.spreadsheetsAppHub.client.receiveName = function (instanceId, name) {
+        if (gdo.clientMode == gdo.CLIENT_MODE.CONTROL) {
+            gdo.consoleOut('.Spreadsheets', 1, 'Instance - ' + instanceId + ": Received Name : " + name);
+        } else if (gdo.clientMode == gdo.CLIENT_MODE.NODE) {
+            gdo.consoleOut('.Spreadsheets', 1, 'Instance - ' + instanceId + ": Received Name : " + name);
+            $("iframe").contents().find("#hello_text").empty().append("Hello " + name);
+        }
+    }
+});
+
+gdo.net.app["Spreadsheets"].initClient = function () {
+    gdo.consoleOut('.Spreadsheets', 1, 'Initializing Spreadsheets App Client at Node ' + gdo.clientId);
+}
+
+gdo.net.app["Spreadsheets"].initControl = function () {
+    gdo.controlId = getUrlVar("controlId");
+    gdo.net.app["Spreadsheets"].server.requestName(gdo.controlId);
+    gdo.consoleOut('.Spreadsheets', 1, 'Initializing Spreadsheets App Control at Instance ' + gdo.controlId);
+
+    $("iframe").contents().find("#hello_submit")
+    .unbind()
+    .click(function () {
+        gdo.consoleOut('.Spreadsheets', 1, 'Sending Name to Clients :' + $("iframe").contents().find('#hello_input').val());
+        gdo.net.app["Spreadsheets"].server.setName(gdo.controlId, $("iframe").contents().find('#hello_input').val());
+    });
+}
+
+gdo.net.app["Spreadsheets"].terminateClient = function () {
+    gdo.consoleOut('.Spreadsheets', 1, 'Terminating Spreadsheets App Client at Node ' + clientId);
+}
+
+gdo.net.app["Spreadsheets"].ternminateControl = function () {
+    gdo.consoleOut('.Spreadsheets', 1, 'Terminating Spreadsheets App Control at Instance ' + gdo.controlId);
+}
---
+++ GDO.Apps.Spreadsheets/SpreadsheetsAppHub.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using GDO.Core;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json.Linq;
+
+namespace GDO.Apps.Spreadsheets
+{
+    [Export(typeof(IAppHub))]
+    public class SpreadsheetsAppHub : Hub, IAppHub
+    {
+        public string Name { get; set; } = "Spreadsheets";
+        public int P2PMode { get; set; } = (int)Cave.P2PModes.None;
+        public Type InstanceType { get; set; } = new SpreadsheetsApp().GetType();
+
+        public void JoinGroup(int instanceId)
+        {
+            Groups.Add(Context.ConnectionId, "" + instanceId);
+        }
+
+        public void ExitGroup(int instanceId)
+        {
+            Groups.Remove(Context.ConnectionId, "" + instanceId);
+        }
+
+        public void SetName(int instanceId, string name)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    ((SpreadsheetsApp)Cave.Apps["Spreadsheets"].Instances[instanceId]).SetName(name);
+                    Clients.Group("" + instanceId).receiveName(instanceId, name);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void RequestName(int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    Clients.Caller.receiveName(instanceId, ((SpreadsheetsApp)Cave.Apps["Spreadsheets"].Instances[instanceId]).GetName());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
