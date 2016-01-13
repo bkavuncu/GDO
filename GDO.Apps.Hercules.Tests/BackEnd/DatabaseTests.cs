e6cd6cc2ca692735e001c653caf210ab8215dabd
Lorenzo Calegari <lmc13@imperial.ac.uk>
2016-01-13 20:38:04 +0000

    Continuing testing

--- GDO.Apps.Hercules.Tests/BackEnd/DatabaseTests.cs
+++ GDO.Apps.Hercules.Tests/BackEnd/DatabaseTests.cs
@@ -18,127 +18,32 @@ namespace GDO.Apps.Hercules.BackEnd.New.Tests
      
         public void go(string name, int expectedRowNo)
         {
-
+            Utils.WriteSeparator();
+            JsonDS ds = Database.JsonDSFromFile("../../TestFiles/WellFormed/" + name, name, "");
+            Debug.WriteLine(ds.schema);
         }
 
         [TestMethod()]
         public void InitTest()
         {
-            //assert.istrue(database.init());
-            //jsonds ds1 = database.jsonfromfile("../../testfiles/test1.csv", "test1", "descriptive description.");
-            //utils.writeseparator();
-            //debug.writeline(ds1);
-            //jsonminiset[] ms = database.queryjsonminisets();
-            //foreach (jsonminiset m in ms) {
-            //    utils.writeseparator();
-            //    debug.writeline(m.tostring());
-            //}
-            //go("askscience.csv", 1000);
-            //go("mdr - tb_burden_estimates_2016 - 01 - 13.csv", 218);
-            //go("sacramentocrimejanuary2006.csv", 7585);
-            //go("sacramentorealestatetransactions.csv", 986);
-            //go("salesjan2009.csv", 999);
-            //go("tb_burden_countries_2016-01-13.csv", 5338);
-            //go("tb_data_dictionary_2016-01-13.csv", 328);
-            //go("tb_hiv_nonroutine_surveillance_2016-01-13.csv", 253);
-            //go("tb_laboratories_2016-01-13.csv", 1299);
-            //go("tb_notifications_2016-01-13.csv", 7458);
-            //go("tb_outcomes_2016-01-13.csv", 4275);
-            //go("tb_strategy_2016-01-13.csv", 218);
-            //go("techcrunchcontinentalusa.csv", 1461);
-
-
-
-
-
-
-
-            //JsonDS ds2 = Database.JsonFromFile("../../TestFiles/test2.csv", "Test2", "The brown fox jumped over the lazy dog#.");
-
-            /* if (ds1 == null && ds2 == null) {
-                Debug.WriteLine(Database.GetError());
-            } else {
-               
-                //string id1 = Database.UploadJsonDS(ds1);
-                string id1 = "565c7fe75e0c5c0d282cee30";
-                JsonMiniset ms1 = Database.QueryJsonMiniset(id1);
-                ++
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                Assert.AreEqual(id1, ms1._id.ToString());
-                JsonRows rs1 = Database.QueryJsonRows(id1);
-                Assert.AreEqual(id1, rs1._id.ToString());
-                ds1 = Database.QueryJsonDS(id1);
-                Assert.AreEqual(id1, ds1.schema._id.ToString());
-
-
-                string id2 = Database.UploadJsonDS(ds2);
-                JsonMiniset ms2 = Database.QueryJsonMiniset(id2);
-                Assert.AreEqual(id2, ms2._id.ToString());
-                JsonRows rs2 = Database.QueryJsonRows(id2);
-                Assert.AreEqual(id2, rs2._id.ToString());
-                ds2 = Database.QueryJsonDS(id2);
-                Assert.AreEqual(id2, ds2.schema._id.ToString());
-
-                JsonMiniset[] mss = Database.QueryJsonMinisets();
-
-            }*/
+            try {
+                Database.Init();
+                go("askscience.csv", 1000);
+                go("MDR-TB_burden_estimates_2016-01-13.csv", 218);
+                go("SacramentocrimeJanuary2006.csv", 7585);
+                go("Sacramentorealestatetransactions.csv", 986);
+                go("SalesJan2009.csv", 999);
+                go("TB_burden_countries_2016-01-13.csv", 5338);
+                go("TB_data_dictionary_2016-01-13.csv", 328);
+                go("TB_hiv_nonroutine_surveillance_2016-01-13.csv", 253);
+                go("TB_laboratories_2016-01-13.csv", 1299);
+                go("TB_notifications_2016-01-13.csv", 7458);
+                go("TB_outcomes_2016-01-13.csv", 4275);
+                go("TB_strategy_2016-01-13.csv", 218);
+                go("TechCrunchcontinentalUSA.csv", 1461);
+            } catch (Exception e) {
+                Debug.WriteLine("OUCH: " + e.Message);
+            }
         }
-
-        [TestMethod()]
-        public void GetMinisetTest()
-        {
-        }
-
-        [TestMethod()]
-        public void GetMinisetsTest()
-        {
-        }
-
-        [TestMethod()]
-        public void GetDatasetTest()
-        {
-        }
-
-        [TestMethod()]
-        public void UploadDSFromRichTest()
-        {
-        }
-
-        [TestMethod()]
-        public void UploadDSFromFileTest()
-        {
-        }
-
-        [TestMethod()]
-        public void UploadDSFromStreamTest()
-        {
-        }
-
-        [TestMethod()]
-        public void UploadDSFromURLTest()
-        {
-        }
-
-        [TestMethod()]
-        public void JsonFromRichTest()
-        {
-        }
-
     }
 }
--- GDO.Apps.Hercules/BackEnd/Database.cs
+++ GDO.Apps.Hercules/BackEnd/Database.cs
@@ -9,16 +9,9 @@ using System.Threading.Tasks;
 
 namespace GDO.Apps.Hercules.BackEnd
 {
-    //
+    // Upload/Delete/Retrieve JsonDS
     public class Database
     {
-
-        // A description of the last error that occurred from calling methods in this class.
-        private static string LastError = "";
-
-        // Returns the last error that occurred from the methods in ServerDS.
-        public static string GetError() { return LastError; }
-
         //
         private static IMongoClient MongoClient;
 
@@ -27,37 +20,33 @@ namespace GDO.Apps.Hercules.BackEnd
 
         //
         private static IMongoCollection<JsonRows> JsonRows;
+
+        //
         private static IMongoCollection<JsonMiniset> JsonMinisets;
 
-
         // Initialization of Database.
-        // Sets up DB connection to MongoDB to database 'datasets'.
-        // If everything wenk OK, returns true.
-        // Otherwise LastError is saved and this returns false.
-        // If Init is called multiple times, make sure that things don't break.
-        public static bool Init()
+        // May throw exceptions!
+        public static void Init()
         {
             MongoClient = new MongoClient("mongodb://localhost/datasets");
             MongoDB = MongoClient.GetDatabase("datasets");
             JsonRows = MongoDB.GetCollection<JsonRows>("jsonrows");
             JsonMinisets = MongoDB.GetCollection<JsonMiniset>("jsonminisets");
-            return true;
+            if (MongoDB == null || JsonRows == null || JsonMinisets == null) {
+                throw new Exception("Something wrong with MongoDB...");
+            }
         }
 
         // Each JsDataset contains the data (rows) and thse JsMiniset (schema).
         // Each JsMiniset has a unique (id) field. We retrieved JsMinisets through this field.
         // Given the ID of a JsMiniset, we need to retrieve it from the MongolDB database. (Good Luck).
-        // Once we obtain it, we need to De?serialize it into a JSON string. Maybe MongolBD can do that 
-        // for us, or we'll need to use Newtonsoft.JSON.
-        // If something goes wrong, return the NULL string and save the error.
+        // May throw exceptions!
         public static JsonMiniset QueryJsonMiniset(string id)
         {
             if (id == null) {
-                LastError = string.Format("Database.GetMiniset({0}): id can't be null!", id);
-                return null;
+                throw new Exception(string.Format("Database.GetMiniset({0}): id can't be null!", id));
             }
 
-            try {
                 // Find the minisets and wait
                 var filter = Builders<JsonMiniset>.Filter.Eq("_id", new ObjectId(id));
                 var task = JsonMinisets.Find(filter).ToListAsync();
@@ -65,47 +54,29 @@ namespace GDO.Apps.Hercules.BackEnd
                 var minisets = task.Result;
 
                 if (minisets.Count <= 0) {
-                    LastError = string.Format("Database.QueryJsonMiniset({0}): no miniset found with this id.", id);
-                    return null;
+                    throw new Exception(string.Format("Database.QueryJsonMiniset({0}): no miniset found with this id.", id));
                 } else if (minisets.Count > 1) {
-                    LastError = string.Format("Database.QueryJsonMiniset({0}): found more than 1 ds with this id... OUCH!", id);
-                    return null;
+                    throw new Exception(string.Format("Database.QueryJsonMiniset({0}): found more than 1 ds with this id... OUCH!", id));
                 } else {
                     return minisets[0];
                 }
-               
-            } catch (Exception ouch) {
-                LastError = string.Format("Database.QueryJsonMiniset({0}): database error ({1}).", id, ouch.Message);
-                return null;
-            }
         }
 
-        // Returns an array containing the JSON strings of ALL the minisets.
-        // If something goes wrong, return the NULL array and save the error.
+        // Returns all the ministes in the database.
+        // May throw exceptions!
         public static JsonMiniset[] QueryJsonMinisets()
         {
-            try {
                 var task = JsonMinisets.Find(_ => true).ToListAsync();
                 task.Wait();
                 var minisets = task.Result; 
     
                 return minisets.ToArray();
-            } catch (Exception ouch) {
-                LastError = string.Format("Database.QueryJsonMinisets: database error ({0}).", ouch.Message);
-                return null;
-            }
         }
 
-        // Each JsDataset contains the data (rows) and the JsMiniset (schema).
-        // Each JsMiniset has a unique (id) field. We retrieved JsMinisets through this field.
-        // Given the ID of a JsMiniset, we need to retrieve it from the MongolDB database. (Good Luck).
-        // But we store the whole JsDatasets so this includes the rows.
-        // Once we obtain it, we need to De?serialize it into a JSON string. Maybe MongolBD can do that 
-        // for us, or we'll need to use Newtonsoft.JSON.
-        // If something goes wrong, return the NULL string and save the error.
+        // Returns the row data given the dataset id.
+        // May throw exceptions!
         public static JsonRows QueryJsonRows(string id)
-        {
-            try {
+        { 
                 // Find the rows and wait
                 var filter = Builders<JsonRows>.Filter.Eq("_id", new ObjectId(id));
                 var task = JsonRows.Find(filter).ToListAsync();
@@ -113,118 +84,91 @@ namespace GDO.Apps.Hercules.BackEnd
                 var rows = task.Result;
 
                 if (rows.Count <= 0) {
-                    LastError = string.Format("Database.QueryJsonRows({0}): no rows found with this id.", id);
-                    return null;
+                    throw new Exception(string.Format("Database.QueryJsonRows({0}): no rows found with this id.", id));
                 } else if (rows.Count > 1) {
-                    LastError = string.Format("Database.QueryJsonRows({0}): found more than 1 set of rows with this id... OUCH!", id);
-                    return null;
+                    throw new Exception(string.Format("Database.QueryJsonRows({0}): found more than 1 set of rows with this id... OUCH!", id));
                 } else {
                     return rows[0];
                 }
-
-            } catch (Exception ouch) {
-                LastError = string.Format("Database.QueryJsonRows({0}): database error ({1})", id, ouch.Message);
-                return null;
-            }
         }
 
+        // Retursn the JsonDS given the dataset id.
+        // May throw exceptions!
         public static JsonDS QueryJsonDS(string id)
         {
-            try {
                 JsonMiniset schema = QueryJsonMiniset(id);
-                if (schema == null)
-                    return null;
-
+ 
                 JsonRows rows = QueryJsonRows(id);
-                if (rows == null)
-                    return null;
 
                 JsonDS ds = new JsonDS();
                 ds.rows = rows.rows;
                 ds.schema = schema;
                 return ds;
-
-            } catch (Exception ouch) {
-                LastError = string.Format("Database.QueryJsonDS({0}): database error ({1})", id, ouch.Message);
-                return null;
-            }
         }
 
-
-        // JsonDS must be set with JsonFromRich
+        // Uploads a JsonDS to the database.
+        // May throw exceptions!
         public static string UploadJsonDS(JsonDS ds)
         {
             if (ds == null) {
-                LastError = "Database.UploadJsonDS: the JsonDS provided is null!";
-                return null;
+                throw new Exception("Database.UploadJsonDS: the JsonDS provided is null!");
             }
 
-            try {
+            // Upload miniset
                 var task = JsonMinisets.Find(ms => ms.name == ds.schema.name).ToListAsync();
                 task.Wait();
-                var minisets = task.Result;
-                
+                var minisets = task.Result;             
                 if (minisets.Count() >= 1) { 
                     ds.schema.name = ds.schema.name + "_copy";
                 }
-
                 JsonMinisets.InsertOneAsync(ds.schema).Wait();
 
+            // Upload rows
                 JsonRows rows = new JsonRows();
                 rows.rows = ds.rows;
-                rows._id = ds.schema._id;
+                rows._id = ds.schema._id; // Override ID
                 JsonRows.InsertOneAsync(rows).Wait();
 
                 string id = ds.schema._id.ToString();
                 return id;
-            } catch (Exception ouch) {
-                LastError = string.Format("Database.UploadDSFromJson: database error ({0})", ouch.Message);
-                return null;
-            }
         }
 
-
-        //
-        public static JsonDS JsonFromURL(string url, string name, string description)
+        // Deletes dataset from the database.
+        // May throw exceptions!
+        public static void DeleteJsonDS(string id)
         {
-            RichDS rich = Augmenter.FromURL(url, ",");
-            if (rich == null) {
-                LastError = "Database.JsonFromURL: " + Augmenter.GetError();
-                return null;
-            }
 
-            return JsonFromRich(rich, name, description, url, "URL");
         }
 
-
-        //
-        public static JsonDS JsonFromFile(string path, string name, string description)
+        // Delets everything from the database.
+        public static void DeleteEverything()
         {
-            RichDS rich = Augmenter.FromFile(path, ",");
-            if (rich == null) {
-                LastError = "Database.JsonFromFile: " + Augmenter.GetError();
-                return null;
-            }
 
-            return JsonFromRich(rich, name, description, path, "FILE");
         }
 
-
-        //
-        public static JsonDS JsonFromStream(Stream stream, string name, string description, string origin, string type)
+        // URL --> JsonDS
+        // May throw exceptions!
+        public static JsonDS JsonDSFromURL(string url, string name, string description)
         {
-            RichDS rich = Augmenter.FromStream(stream, ",");
-            if (rich == null) {
-                LastError = "Database.JsonFromStream: " + Augmenter.GetError();
-                return null;
-            }
-
-            return JsonFromRich(rich, name, description, origin, type);
+            return JsonDSFromRich(Augmenter.FromURL(url, ","), name, description, url, "URL");
         }
 
+        // Path --> JsonDS
+        // May throw exceptions!
+        public static JsonDS JsonDSFromFile(string path, string name, string description)
+        {
+            return JsonDSFromRich(Augmenter.FromFile(path, ","), name, description, path, "FILE");
+        }
+
+        // Stream --> JsonDS
+        // May throw exceptions!
+        public static JsonDS JsonDSFromStream(Stream stream, string name, string description, string origin, string type)
+        {
+            return JsonDSFromRich(Augmenter.FromStream(stream, ","), name, description, origin, type);
+        }
 
         // RichDS --> JsonDS
-        public static JsonDS JsonFromRich(RichDS rich, string name, string description, string origin, string type)
+        public static JsonDS JsonDSFromRich(RichDS rich, string name, string description, string origin, string type)
         {
             JsonDS json = new JsonDS();
             json.rows = rich.Rows;
@@ -258,68 +202,3 @@ namespace GDO.Apps.Hercules.BackEnd
         }
     }
 }
-
-
-
-//// Given the path of a file, a name for the dataset and a description for a dataset,
-//// opens the file, parse the file and obtain a JsDataset. The parsing is done by
-//// RichDS. If RichDS fails, obtain the error that occurred and save it as this LastError.
-//// If name is null or empty, use the file name from path. 
-//// CHECK that the name is unique in the database, otherwise come up with a new name.
-//// If description is null or empty, use "No description provided".
-//// If the JsDataset ds is obtained, serialize it into a BSON document and save it into
-//// the database.
-//// Then obtain the *automagically* unique ID for it and RETURN it.
-//// If stuff goes wrong, save the error and return -1.
-//public static string UploadDSFromFile(string path, string name, string description)
-//{
-//    RichDS rich = Augmenter.FromFile(path, ",");
-//    if (rich == null) {
-//        LastError = "Database.UploadDSFromFile: " + Augmenter.GetError();
-//        return null;
-//    }
-
-//    return UploadDSFromRich(rich, path, name, description);
-//}
-
-
-////
-//public static string UploadDSFromJson(JsonDS json)
-//{
-//    try {
-//        BsonDocument doc = json.ToJ
-//                Task fuck = JsonDatasets.InsertOneAsync(doc);
-//        fuck.Wait();
-//        doc.
-//                return doc["schema._id"].ToString();
-//    } catch (Exception ouch) {
-//        LastError = string.Format("Database.UploadDSFromJson: database error ({0})", ouch.Message);
-//        return null;
-//    }
-//}
-
-
-////
-//public static string UploadDSFromStream(Stream stream, string name, string description)
-//{
-//    RichDS rich = Augmenter.FromStream(stream, ",");
-//    if (rich == null) {
-//        LastError = "Database.UploadDSFromStream: " + Augmenter.GetError();
-//        return null;
-//    }
-
-//    return UploadDSFromRich(rich, "<stream>", name, description);
-//}
-
-
-////
-//public static string UploadDSFromURL(string url, string name, string description)
-//{
-//    RichDS rich = Augmenter.FromURL(url, ",");
-//    if (rich == null) {
-//        LastError = "Database.UploadDSFromFile: " + Augmenter.GetError();
-//        return null;
-//    }
-
-//    return UploadDSFromRich(rich, url, name, description);
-//}
