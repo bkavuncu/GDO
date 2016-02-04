19f157964767ebe33815e55fb1fb42764d674e91
Siddharth Vasu <siddharth_vasu@hotmail.co.uk>
2016-02-04 22:34:18 +0000

    working ViewModel button that loads up a spreadsheet on every console application that is connected.

--- GDO.Apps.Spreadsheets/Scripts/Spreadsheets/gdo.apps.Spreadsheets.js
+++ GDO.Apps.Spreadsheets/Scripts/Spreadsheets/gdo.apps.Spreadsheets.js
@@ -18,10 +18,11 @@ gdo.net.app["Spreadsheets"].initClient = function () {
 }
 
 gdo.net.app["Spreadsheets"].initControl = function () {
+    gdo.loadModule('viewModel', 'Spreadsheets', gdo.MODULE_TYPE.APP);
     gdo.controlId = getUrlVar("controlId");
     gdo.net.app["Spreadsheets"].server.requestName(gdo.controlId);
-    gdo.consoleOut(".Spreadsheets", 1, "Initializing Spreadsheets App Control at Instance " + gdo.controlId);
-
+    gdo.consoleOut(".Spreadsheets", 1, "Initializing Spreadsheets App Con trol at Instance " + gdo.controlId);
+    $("iframe").contents().find('#operation').hide();
     $("iframe").contents().find("#spreadsheet_submit")
     .unbind()
     .click(function () {
@@ -32,7 +33,29 @@ gdo.net.app["Spreadsheets"].initControl = function () {
                 if (spreadSheet.type === type && config.type === type) {
                     gdo.net.app["Spreadsheets"].server.setName(gdo.controlId, spreadSheet.name + " <br /> " + config.name);
                     gdo.consoleOut(".Spreadsheets", 1, "Set Name, now calling FileAdded on Server");
-                    gdo.consoleOut(".Spreadsheets", 1, "called function.");
+                    $("iframe").contents().find("#file_upload_form").unbind('submit').submit(function (event) {
+                        event.preventDefault();
+                        var formData = new FormData();
+                        formData.append('files', spreadSheet);
+                        formData.append('files', config);
+                        gdo.consoleOut(".Spreadsheets", 1, "Submitting form to Spreadsheet Server");
+                        $.ajax({
+                            url: 'http://localhost:1395/File/Upload',
+                            type: 'POST',
+                            data: formData,
+                            success: function (message) {
+                                gdo.consoleOut(".Spreadsheets", 1, message.message);
+                                $('iframe').contents().find('#uploaded_files').html(message.data[0].Filename +  "," + message.data[1].Filename);
+                                $('iframe').contents().find('#operation').show();
+                                $('iframe').contents().find('#view_model').unbind().click(function() {
+                                    gdo.consoleOut('.Spreadsheets', 1, "viewModel selected, now to run task.");
+                                    gdo.net.app["Spreadsheets"].viewModel(gdo.controlId,message.data[0], message.data[1]);
+                                });
+                            },
+                            processData: false,
+                            contentType: false
+                        });
+                    });
                     $("iframe").contents().find("#file_upload_form").submit();
                 } else {
                     alert("files should be of type .xls or .xlsx");
---
+++ GDO.Apps.Spreadsheets/Scripts/Spreadsheets/gdo.apps.Spreadsheets.viewModel.js
@@ -0,0 +1,10 @@
+﻿gdo.net.app["Spreadsheets"].viewModel = function (id , model, config) {
+	$.ajax({
+	    url: "http://localhost:1395/Operations/ViewModel",
+        method: "POST",
+	    data: {instanceId:id, model: model, config: config },
+		success: function(message) {
+		    gdo.consoleOut(".Spreadsheets", 1, JSON.stringify(message));
+		}
+    });
+}
