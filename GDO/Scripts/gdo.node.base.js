0915050f6849a6ed3032eef912ff4095bf9b41dc
bkavuncu <bkavuncu@users.noreply.github.com>
2016-03-11 11:28:25 +0000

    base frame font problem fix?

--- GDO/Scripts/gdo.node.base.js
+++ GDO/Scripts/gdo.node.base.js
@@ -1,9 +1,9 @@
-﻿$(function () {
+$(function () {
     gdo.consoleOut(".BASE", 2, "Loaded Base Frame");
     if (gdo.clientMode == gdo.CLIENT_MODE.NODE) {
         $('iframe').contents().find("#base_title").empty();
         if (gdo.net.blankMode == false || gdo.net.blankMode == null) {
-            $('iframe').contents().find("#base_title").css("fontSize", "77vh").css("color", "#222").append("<b>" + gdo.clientId + "</b>").css("");
+            $('iframe').contents().find("#base_title").css("fontSize", "77vh").css("font-family", "fontawesome").css("color", "#222").append("<font face='fontawesome'><b>" + gdo.clientId + "</b></font>").css("");
         }
     }
 });
@@ -14,7 +14,7 @@ gdo.initBaseFrame = function ()
     if (gdo.clientMode == gdo.CLIENT_MODE.NODE) {
         $('iframe').contents().find("#base_title").empty();
         if (gdo.net.blankMode == false) {
-            $('iframe').contents().find("#base_title").css("fontSize", "77vh").css("color", "#222").append("<b>" + gdo.clientId + "</b>");
+            $('iframe').contents().find("#base_title").css("fontSize", "77vh").css("font-family", "fontawesome").css("color", "#222").append("<font face='fontawesome'><b>" + gdo.clientId + "</b></font>");
         } 
     } 
 }
