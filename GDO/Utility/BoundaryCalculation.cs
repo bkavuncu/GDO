1a1b36f0946c69f170efa2c0e2b46aad42ec423c
Bertan Kavuncu <bertankavuncu@gmail.com>
2016-04-11 15:49:06 +0100

    boundary calculation variables

--- GDO/Utility/BoundaryCalculation.cs
+++ GDO/Utility/BoundaryCalculation.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GDO.Core;
+using GDO.Core.Apps;
 
 namespace GDO.Utility
 {
     public class BoundaryCalculation
     {
+        public int SectionWidth { get; set; }
+        public int SectionHeight { get; set; }
+
+        public void Init(int instanceId)
+        {
+            SectionWidth = ((IBaseAppInstance)Cave.Instances[instanceId]).Section.Width;
+            SectionHeight = ((IBaseAppInstance)Cave.Instances[instanceId]).Section.Height;
+        }
+
+        public Boundary CalculateBoundary(double centerX, double centerY, double pixelToUnitRatio)
+        {
+            //TODO
+            return null;
+        }
+
+        public class Boundary
+        {
+            public double TopLeftX { get; set; }
+            public double TopLeftY { get; set; }
+            public double CenterX { get; set; }
+            public double CenterY { get; set; }
+            public double BottomRightX { get; set; }
+            public double BottomRightY { get; set; }
+            public double Resolution { get; set; }
+        }
     }
 }
