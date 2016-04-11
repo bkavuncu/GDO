c6c962d2978255d6a5cb13d0558592931a1d32eb
Bertan Kavuncu <bertankavuncu@gmail.com>
2016-04-11 15:55:56 +0100

    boundary calculation function

--- GDO/Utility/BoundaryCalculation.cs
+++ GDO/Utility/BoundaryCalculation.cs
@@ -20,8 +20,11 @@ namespace GDO.Utility
 
         public Boundary CalculateBoundary(double centerX, double centerY, double pixelToUnitRatio)
         {
-            //TODO
-            return null;
+            double topLeftX = centerX - ((SectionWidth*pixelToUnitRatio)/2);
+            double topLeftY = centerY + ((SectionHeight * pixelToUnitRatio) / 2);
+            double bottomRightX = centerX + ((SectionWidth * pixelToUnitRatio) / 2);
+            double bottomRightY = centerY - ((SectionHeight * pixelToUnitRatio) / 2);
+            return new Boundary(topLeftX,topLeftY,centerX,centerY,bottomRightX,bottomRightY,pixelToUnitRatio);
         }
 
         public class Boundary
@@ -33,6 +36,18 @@ namespace GDO.Utility
             public double BottomRightX { get; set; }
             public double BottomRightY { get; set; }
             public double Resolution { get; set; }
+
+
+            public Boundary(double topLeftX, double topLeftY, double centerX, double centerY, double bottomRightX, double bottomRightY, double resolution)
+            {
+                TopLeftX = topLeftX;
+                TopLeftY = topLeftY;
+                CenterX = centerX;
+                CenterY = centerY;
+                BottomRightX = bottomRightX;
+                BottomRightY = bottomRightY;
+                Resolution = resolution;
+            }
         }
     }
 }
