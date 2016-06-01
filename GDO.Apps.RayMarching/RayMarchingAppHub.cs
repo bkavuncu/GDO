24a3b84d7f34b327825d9cbcc0e2642bc5055066
Dan Thomas <Dan Thomas>
2016-06-01 23:01:23 +0100

    Stripped down fractals project to create blank ray marching library

---
+++ GDO.Apps.RayMarching/RayMarchingAppHub.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using GDO.Core;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System.Threading;
+namespace GDO.Apps.RayMarching
+{
+    [Export(typeof(IAppHub))]
+    public class RayMarchingAppHub : Hub, IBaseAppHub
+    {
+        public string Name { get; set; } = "RayMarching";
+        public int P2PMode { get; set; } = (int)Cave.P2PModes.None;
+        public Type InstanceType { get; set; } = new RayMarchingApp().GetType();
+
+        public void JoinGroup(int instanceId)
+        {
+            Groups.Add(Context.ConnectionId, "" + instanceId);
+        }
+
+        public void ExitGroup(int instanceId)
+        {
+            Groups.Remove(Context.ConnectionId, "" + instanceId);
+        }
+
+        public JsonSerializerSettings JsonSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All};
+
+        public void SendParams(int instanceId)
+        {
+            try
+            {
+                RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                if (!FA.Sync)
+                {
+                    string Json = Newtonsoft.Json.JsonConvert.SerializeObject(FA, JsonSettings);
+                    Clients.Group("" + instanceId).updateParams(instanceId, Json);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void JoystickInit(int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.JoystickInit(this, instanceId);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void JoystickReceiveParamsRot(int instanceId, float angle, float magnitude)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.JoystickUpdateParamsRot(angle, magnitude);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void JoystickReceiveParamsMove(int instanceId, float angle, float magnitude)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.JoystickUpdateParamsMove(angle, magnitude);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void JoystickTerminate(int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.JoystickTerminate();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void HeightSliderReceiveParamsMove(int instanceId, float val)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.HeightSliderUpdateParamsMove(val);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+
+
+
+
+
+        public void SyncTime(int instanceId, int val)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.SyncTime = val;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void SyncToggle(int instanceId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    FA.Sync = !FA.Sync;
+                    string Json = Newtonsoft.Json.JsonConvert.SerializeObject(FA, JsonSettings);
+                    Clients.Group("" + instanceId).updateParams(instanceId, Json);
+                    Clients.Group("" + instanceId).renderNextFrame(instanceId, FA.CurrentFrame);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void AckFrameRendered(int instanceId)
+        {
+            if (instanceId == -1) return;
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    if (FA.rendering)
+                    {
+                        
+                        FA.Acks++;
+
+                        if (FA.Acks >= FA.Nodes)
+                        {
+                            FA.rendering = false;
+                            FA.swapping = true;
+                            FA.CurrentFrame = (FA.CurrentFrame + 1) % 2;
+
+                            string Json = Newtonsoft.Json.JsonConvert.SerializeObject(FA, JsonSettings);
+
+                            Clients.Group("" + instanceId).swapFrame(instanceId, Json, (long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds + FA.SyncTime);
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void AckSwapFrame(int instanceId)
+        {
+            if (instanceId == -1) return;
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    if (FA.swapping)
+                    {
+                        
+                        FA.SwapFrameAcks++;
+
+                        if (FA.SwapFrameAcks >= FA.Nodes)
+                        {
+                            FA.swapping = false;
+                            FA.rendering = true;
+                            FA.Acks = 0;
+                            FA.SwapFrameAcks = 0;
+                            FA.Nodes += FA.NewNodes;
+                            FA.NewNodes = 0;
+                            Clients.Group("" + instanceId).renderNextFrame(instanceId, FA.CurrentFrame);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void IncNodes(int instanceId, int clientId)
+        {
+            if (instanceId == -1) return;
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    if (FA.NodesOnline[clientId] != 1) {
+                        if (FA.NewNodes == 0 && FA.Nodes == 0)
+                        {
+                            // Start synch cycle
+                            FA.Nodes = 1;
+                            Clients.Caller.renderNextFrame(instanceId, FA.CurrentFrame);
+                        }
+                        else
+                        {
+                            // Add new nodes after current cycle
+                            FA.NewNodes++;
+                        }
+                        FA.NodesOnline[clientId] = 1;
+                    } else
+                    {
+                        FA.rendering = true;
+                        FA.swapping = false;
+                        FA.Acks = 0;
+                        FA.SwapFrameAcks = 0;
+                        // Refreshed page, render relevant frame
+                        if (FA.Sync)
+                        {
+                            Clients.Group("" + instanceId).renderNextFrame(instanceId, FA.CurrentFrame);
+                        } else
+                        {
+                            Clients.Group("" + instanceId).renderNextFrameNoSync();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void DecNodes(int instanceId, int clientId)
+        {
+            lock (Cave.AppLocks[instanceId])
+            {
+                try
+                {
+                    RayMarchingApp FA = ((RayMarchingApp)Cave.Apps["RayMarching"].Instances[instanceId]);
+                    if (FA.NodesOnline[clientId] == 1)
+                    {
+                        FA.Nodes--;
+                        FA.NodesOnline[clientId] = 0;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+    }
+}
---
+++ GDO.Apps.RayMarching/Scripts/RayMarching/gdo.apps.RayMarching.webgl.js
@@ -0,0 +1,234 @@
+﻿var gl1;
+var program1;
+var canvas1;
+
+var gl2;
+var program2;
+var canvas2;
+
+var sync;
+
+function initWebgl(id, locations, shader, completeInit) {
+
+    var gl;
+    var program;
+
+    // Set up canvas
+    var canvas = $("iframe").contents().find(id)[0];
+    gl = canvas.getContext('experimental-webgl');
+    //canvas.width = 480;
+    canvas.width = 960;
+    canvas.height = 540;
+    //canvas.width = 1920;
+    //canvas.height = 1080;
+
+    // Initialise view port
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    // Create a quad filling the screen
+    buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+        -1.0, -1.0,
+         1.0, -1.0,
+        -1.0, 1.0,
+        -1.0, 1.0,
+         1.0, -1.0,
+         1.0, 1.0]),
+      gl.STATIC_DRAW
+    );
+
+    // Shader local variables
+    var shaderScript;
+    var shaderSource;
+    var vertexShader;
+    var fragmentShader;
+
+    // Compile vertex shader
+    shaderScript = $("iframe").contents().find("#2d-vertex-shader")[0];
+    shaderSource = shaderScript.text;
+    vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, shaderSource);
+    gl.compileShader(vertexShader);
+
+    // Compile fragment shader
+    //shaderScript = $("iframe").contents().find(shader)[0];
+    //shaderSource = shaderScript.text;
+    //fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    //gl.shaderSource(fragmentShader, shaderSource);
+    //gl.compileShader(fragmentShader);
+
+    loadFiles(['../scripts/RayMarching/Shaders/rayMarch.js', '../scripts/RayMarching/Shaders/init.js'], function (shaderText) {
+        gdo.consoleOut('.RayMarching', 1, shaderText[0] + shaderText[1]);
+        // Compile fragment shader
+        fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader, shaderText[0] + shaderText[1]);
+        gl.compileShader(fragmentShader);
+
+        // Create program with shaders
+        program = gl.createProgram();
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+        gl.useProgram(program);
+
+        // Transparent background
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Turn on alpha
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+        gl.enable(gl.BLEND);
+        gl.disable(gl.DEPTH_TEST);
+
+        // Setup rotations
+        locations.xRotLoc = gl.getUniformLocation(program, "xRot");
+        gl.uniform1f(locations.xRotLoc, parameters.xRot);
+        locations.yRotLoc = gl.getUniformLocation(program, "yRot");
+        gl.uniform1f(locations.yRotLoc, parameters.yRot);
+
+        // Setup focal
+        var focalLoc = gl.getUniformLocation(program, "focal");
+        gl.uniform1f(focalLoc, 1 / Math.tan((315 / 32) * (Math.PI / 180)));
+
+        // Setup translation
+        locations.transLoc = gl.getUniformLocation(program, "translation");
+        gl.uniform3f(locations.transLoc, parameters.xTrans, parameters.yTrans, parameters.zTrans);
+
+        // Setup eye
+        locations.eyeLoc = gl.getUniformLocation(program, "eyeHeight");
+        gl.uniform1f(locations.eyeLoc, -parameters.yHeight);
+
+        // Set page size
+        locations.widthloc = gl.getUniformLocation(program, "width");
+        gl.uniform1f(locations.widthloc, canvas.width);
+        locations.heightloc = gl.getUniformLocation(program, "height");
+        gl.uniform1f(locations.heightloc, canvas.height);
+
+        completeInit(gl, program, canvas);
+
+    }, function (url) {
+        alert('Failed to download "' + url + '"');
+    });
+
+
+
+    // Render the scene
+    //render(locations, gl, program);
+
+    //return { gl: gl, program: program, canvas: canvas };
+}
+
+function loadFile(url, data, callback, errorCallback) {
+    // Set up an asynchronous request
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    // Hook the event that gets called as the request progresses
+    request.onreadystatechange = function () {
+        // If the request is "DONE" (completed or failed)
+        if (request.readyState == 4) {
+            // If we got HTTP status 200 (OK)
+            if (request.status == 200) {
+                callback(request.responseText, data)
+            } else { // Failed
+                alert(request.status);
+                errorCallback(url);
+            }
+        }
+    };
+
+    request.send(null);
+}
+
+function loadFiles(urls, callback, errorCallback) {
+    var numUrls = urls.length;
+    var numComplete = 0;
+    var result = [];
+
+    // Callback for a single file
+    function partialCallback(text, urlIndex) {
+        result[urlIndex] = text;
+        numComplete++;
+
+        // When all files have downloaded
+        if (numComplete == numUrls) {
+            callback(result);
+        }
+    }
+
+    for (var i = 0; i < numUrls; i++) {
+        loadFile(urls[i], i, partialCallback, errorCallback);
+    }
+}
+
+rendering = false;
+function renderSync(locations, gl, program) {
+    
+    if (!rendering) {
+        rendering = true;
+
+        if (sync) {
+
+            // Ensure continuous rendering
+            window.requestAnimationFrame(function () {
+                rendering = false;
+                gdo.net.app["RayMarching"].server.ackFrameRendered(gdo.net.node[gdo.clientId].appInstanceId);
+            });
+
+            // Apply params
+            applyParams(locations, gl);
+
+            // Set position data of vertex shader
+            positionLocation = gl.getAttribLocation(program, "a_position");
+            gl.enableVertexAttribArray(positionLocation);
+            gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+            // Draw quad
+            gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        } else {
+            rendering = false;
+            canvas1.style.zIndex = 5;
+            canvas2.style.zIndex = 0;
+            render(locations1, gl1, program1);
+        }
+    } else {
+        gdo.consoleOut('.RayMarching', 1, 'Multiple Renders Detected');
+    }
+}
+rendering2 = false;
+function render(locations, gl, program) {
+
+    if (!rendering2) {
+        rendering2 = true;
+
+        if (!sync) {
+
+            // Ensure continuous rendering
+            window.requestAnimationFrame(function () {
+                rendering2 = false;
+                render(locations, gl, program);
+            });
+
+            // Apply params
+            applyParams(locations, gl);
+
+            // Set position data of vertex shader
+            positionLocation = gl.getAttribLocation(program, "a_position");
+            gl.enableVertexAttribArray(positionLocation);
+            gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+            // Draw quad
+            gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        } else {
+            rendering2 = false;
+            renderSync(locations2, gl2, program2);
+        }
+    } else {
+        gdo.consoleOut('.RayMarching', 1, 'Multiple Renders Detected');
+    }
+}
