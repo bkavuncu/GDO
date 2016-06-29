9b3786278501fdcbb1bc42a16cc2dcb7e504365a
T0ll <g.paillot@gmail.com>
2016-06-29 11:00:27 +0200

    added Benchmark tool and started to comment backend code

--- GDO.Apps.DD3/DD3App.cs
+++ GDO.Apps.DD3/DD3App.cs
@@ -12,15 +12,27 @@ using Newtonsoft.Json;
 
 namespace GDO.Apps.DD3
 {
+    //Class which represents the application instance in the GDO framework
     public class DD3App : IBaseAppInstance
     {
+        //Id of the application instance
         public int Id { get; set; }
+        //Name of the application instance
         public string AppName { get; set; }
+        //Section which contains the application instance
         public Section Section { get; set; }
+        //Configuration (i.e. visualization) launched in this instance
         public AppConfiguration Configuration { get; set; }
+        //Unknown and unused in DD3
         public bool IntegrationMode { get; set; }
+        //Advanced App instance if launched by another advanced app => unused, see wiki if you want to create that
         public IAdvancedAppInstance ParentApp { get; set; }
 
+        /*
+         * Initialisation function
+         * Initialise data, configuration and directory
+         * In particular, retrieve the json configuration object
+         */
         public void Init()
         {
             this.Context = (IHubContext<dynamic>) GlobalHost.ConnectionManager.GetHubContext<DD3AppHub>();
@@ -31,8 +43,10 @@ namespace GDO.Apps.DD3
             this.ConfigurationId = (int)value;
 
             data = new Data(this.Configuration.Name);
+            Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/Web/Images/images/dd3"));
+
             /*
-            // Test For data request
+            // Test For data request - unused, but can be leveraged if connection to database needs to be developped
 
             var server = "http://wikisensing.org/WikiSensingServiceAPI/DCEWilliamPenneyUpperFloorzYyYFkLwEygj7B0vvQWQ/Node_1/5";
             String[] toArray = { "sensorRecords" };
@@ -52,56 +66,70 @@ namespace GDO.Apps.DD3
             webRequest("id", server, toArray, toObject, toValues, useNames3);
             */
 
-            Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/Web/Images/images/dd3"));
+
         }
 
+        //Dictionnary of browser nodes browser info by id
         private ConcurrentDictionary<string, BrowserInfo> browserList = new ConcurrentDictionary<string, BrowserInfo>();
+        //Dictionnary of browsrer nodes  sync state (true: synchronised, false: not synchronized) by id
         private ConcurrentDictionary<string, bool> syncList = new ConcurrentDictionary<string, bool>();
+        //Locker used for making sure we are not creating and removing a client at the same time
         private readonly object _locker = new Object();
+        //Object where information about the DD3AppHub is stored
         private IHubContext<dynamic> Context;
+
+        //Id of the controller node (i.e. tablet in the observatory)
         private string controllerId = "";
+        //Id of the configuration (could be retrieved from the configuration ?)
         private int ConfigurationId;
+        //Data object
         private Data data;
 
 
         // Code for Data Request and Management
 
+        //return dimensions of the Data object for a given dataid
         public string getDimensions(string dataId)
         {
             return data.getDimensions(dataId).ToString();
         }
 
+        //Request data from the data object based on a DataRequest
         public string requestData(DataRequest request)
         {
             var dataIn = this.data.getById(request.dataId);
             return dataIn == null ? "{\"error\":\"incorrect_dataId\"}" : JsonConvert.SerializeObject(request.createResponseObject(dataIn));
         }
 
+        //Request data from the data object based on a PointDataRequest
         public string requestPointData(PointDataRequest request)
         {
             var dataIn = this.data.getById(request.dataId);
             return dataIn == null ? "{\"error\":\"incorrect_dataId\"}" : request.executeWith(dataIn);
         }
 
+        //Request data from the data object based on a PathDataRequest
         public string requestPathData(PathDataRequest request)
         {
             var dataIn = this.data.getById(request.dataId);
             return dataIn == null ? "{\"error\":\"incorrect_dataId\"}" : request.executeWith(dataIn);
         }
 
+        //Request data from the data object based on a Path Data Request
         public string requestBarData(BarDataRequest request)
         {
             var dataIn = this.data.getById(request.dataId);
             return dataIn == null ? "{\"error\":\"incorrect_dataId\"}" : request.executeWith(dataIn);
         }
 
+        //Request data from the data object based on a Remote Data Request
         public string requestRemoteData(RemoteDataRequest request)
         {
             return request.execute(this.data);
         }
 
         // Code for Connection and Control
-
+        //Initialise a new application node
         public void newClient(string cid, BrowserInfo b)
         {
             lock (_locker)
@@ -118,6 +146,7 @@ namespace GDO.Apps.DD3
             }
         }
 
+        // Define which node is the controller
         public void defineController (string id)
         {
             controllerId = id;
