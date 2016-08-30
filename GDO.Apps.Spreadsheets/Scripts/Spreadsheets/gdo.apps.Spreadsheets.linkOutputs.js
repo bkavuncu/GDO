c983e17997ac09648c28013c93d11d8d3949d18e
davidbirch <david.birch@imperial.ac.uk>
2016-08-30 21:03:10 +0100

    Code Quality on Spreadsheets

--- GDO.Apps.Spreadsheets/Scripts/Spreadsheets/gdo.apps.Spreadsheets.linkOutputs.js
+++ GDO.Apps.Spreadsheets/Scripts/Spreadsheets/gdo.apps.Spreadsheets.linkOutputs.js
@@ -7,16 +7,24 @@ var obtainFormulae = function (id, attemptNo) {
         url: conf.serverAddress + "Operations/CheckFormulae",
         method: "GET",
         data: { instanceId: id },
-        success : function(response) {
+        success: function(response) {
             if (response.success) {
                 console.log(response);
-                $('iframe').contents().find('#link_result').html("<p style=\"color:green\">Successfully obtained formulae.</p>");
+                $('iframe')
+                    .contents()
+                    .find('#link_result')
+                    .html("<p style=\"color:green\">Successfully obtained formulae.</p>");
             } else {
-                $('iframe').contents().find('#link_result').html("<p style=\"color:red\">Attempt " + attemptNo + ". Could not get formulae, trying again.</p>");
+                $('iframe')
+                    .contents()
+                    .find('#link_result')
+                    .html("<p style=\"color:red\">Attempt " +
+                        attemptNo +
+                        ". Could not get formulae, trying again.</p>");
                 setTimeout(function() { obtainFormulae(id, attemptNo + 1) }, 500);
             }
         }
-    })
+    });
 }
 
 gdo.net.app["Spreadsheets"].linkOutputs = function (id, model, config, section) {
@@ -24,13 +32,16 @@ gdo.net.app["Spreadsheets"].linkOutputs = function (id, model, config, section) 
         url: conf.serverAddress + "Operations/SetupLinkOutputs",
         method: "POST",
         data: { instanceId: id, model: model, config: config },
-        success: function (response) {
+        success: function(response) {
             if (response.success) {
                 gdo.consoleOut(".Spreadsheets", 1, response.message);
                 obtainFormulae(id, 1);
                 gdo.consoleOut(".Spreadsheets", 1, "Now to obtain formulae information from server.");
             } else {
-                $('iframe').contents().find('#view_model_message').html("<p style=\"color:red\">Exception[SetupLinkOutputs]:" + response.message + "</p>");
+                $('iframe')
+                    .contents()
+                    .find('#view_model_message')
+                    .html("<p style=\"color:red\">Exception[SetupLinkOutputs]:" + response.message + "</p>");
                 gdo.consoleOut(".Spreadsheets", 1, "Exception[SetupLinkOutputs]:" + response.message);
             }
         }
