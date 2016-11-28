f92db6bb9570dd22e929044f8cdc45d30765aa9a
davidbirch <david.birch@imperial.ac.uk>
2016-11-28 09:43:10 +0000

    mmm pie!
    this is now working :-)

--- GDO/GDOController.cs
+++ GDO/GDOController.cs
@@ -1,12 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using GDO.Core;
+using log4net;
 using Microsoft.AspNet.SignalR;
 
 namespace GDO
 {
+
+    public sealed class GDOAPISingleton {
+
+        public CaveHub Hub;
+    
+
+        private static GDOAPISingleton _instance;
+        private static readonly object Padlock = new object();
+
+        public DateTime LastCommandSent = DateTime.Now;
+
+        private ConcurrentQueue<string> _logMessages = new ConcurrentQueue<string>();
+        private GDOAPISingleton() {
+        }
+
+        public static GDOAPISingleton Instance
+        {
+            get
+            {
+                lock (Padlock) {
+                    return _instance ?? (_instance = new GDOAPISingleton());
+                }
+            }
+        }
+
+        public void Log(string message) {
+            _logMessages.Enqueue(DateTime.Now + " : " + message);
+        }
+        public void ClearLog() {
+            _logMessages = new ConcurrentQueue<string>();
+        }
+
+        public string GetLogMessages() {
+            return _logMessages.ToArray().Aggregate("Log Messages " + _logMessages.Count, (acc, next) => acc + "\n" + next);
+        }
+    }
+
     public class GDOController : ApiController
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(GDOController));
+
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
@@ -18,9 +61,14 @@ namespace GDO
         public string Get(int id)
         {
             //Cave.ClearCave();
-            IHubContext cavehub = GlobalHost.ConnectionManager.GetHubContext<CaveHub>();
+            // IHubContext<CaveHub> cavehub = GlobalHost.ConnectionManager.GetHubContext<CaveHub>("CaveHub");
+            // cavehub.Clients.All.SetMaintenanceMode(true);
             //cavehub.ClearCave();
-            return "Thank you Senaka! " +id;
+
+            GDOAPISingleton.Instance.Hub.ClearCave();
+
+            return GDOAPISingleton.Instance.Hub == null ? "fish" : "pie";
+           return "Thank you Senaka! " +id;
         }
 
         // POST api/<controller>
