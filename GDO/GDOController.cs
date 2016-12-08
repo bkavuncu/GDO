7d93241550d0e5a6d3b955b4c556b83d6ef9437b
davidbirch <david.birch@imperial.ac.uk>
2016-12-08 16:01:27 +0000

    code review

--- GDO/GDOController.cs
+++ GDO/GDOController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using GDO.Core;
 using log4net;
-using Microsoft.AspNet.SignalR;
 
 namespace GDO
 {
@@ -50,15 +48,9 @@ namespace GDO
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(GDOController));
 
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // http://localhost:12332/api/gdo/0
+        // http://localhost:12332/api/gdo/
         // GET api/<controller>/5
-        public string Get(int id)
+        public string ClearCave()
         {
             //Cave.ClearCave();
             // IHubContext<CaveHub> cavehub = GlobalHost.ConnectionManager.GetHubContext<CaveHub>("CaveHub");
@@ -67,23 +59,8 @@ namespace GDO
 
             GDOAPISingleton.Instance.Hub.ClearCave();
 
-            return GDOAPISingleton.Instance.Hub == null ? "fish" : "pie";
-           return "Thank you Senaka! " +id;
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            return GDOAPISingleton.Instance.Hub == null ? "hub Conected " : "hub not connected";
+           
         }
     }
 }
