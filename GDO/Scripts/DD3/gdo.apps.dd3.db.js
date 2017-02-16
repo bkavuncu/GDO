5738f022bcee39e8a410559e75696e05637b997e
shabai517\shabai517 <shabai517@163.com>
2017-02-17 00:29:41 +0800

    create database api template for xiaoping

---
+++ GDO/Scripts/DD3/gdo.apps.dd3.db.js
@@ -0,0 +1,23 @@
+﻿//$(function () {
+//    $.getScript('gdo.apps.dd3.db.js', callback);
+//});
+
+
+var db = {
+    config: {
+        host: aaa,
+        port: aaa,
+        dbname: aaa,
+        username: aaa,
+        password: aaa,
+        options: {}
+    },
+
+    find: function () { },
+    connection: function() {}
+
+};
+
+
+
+
