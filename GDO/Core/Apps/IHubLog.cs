9bd70a527235e01262c32511607743107b6f5844
davidbirch <david.birch@imperial.ac.uk>
2017-02-17 12:06:50 +0000

    Added file missed in a previous commit

---
+++ GDO/Core/Apps/IHubLog.cs
@@ -0,0 +1,14 @@
+﻿using log4net;
+
+namespace GDO.Core.Apps
+{
+	/// <summary>
+	/// An interace to force hubs to support Logging, 
+	/// todo eventually this will be taged on the Hub class forcing all hubs to implement logging 
+	/// </summary>
+	public interface IHubLog
+	{
+		ILog Log { get; set; } 
+
+	}
+}
