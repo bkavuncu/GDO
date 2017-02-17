c57efeb7f177d3bb44f3e27e7955249f5792eb31
shabai517\shabai517 <shabai517@163.com>
2017-02-18 00:18:44 +0800

    database template for xiaoping

---
+++ GDO.Apps.DD3/Scripts/DD3/gdo.apps.dd3.db.js
@@ -0,0 +1,26 @@
+﻿//template for xiaoping
+
+var db = {
+    config: {
+        host: aaa,
+        port: aaa,
+        dbname: aaa,
+        username: aaa,
+        password: aaa,
+        options: {}
+    },
+    //Add data to database
+    insert: function (collection, data) { },
+    //Delete data in the database
+    delete: function (collection, data) { },
+    //Update new data to the database
+    update: function (collection, query, data) { },
+    // find a data from database.
+    find: function (collection, query) { }
+    
+
+};
+
+
+
+
