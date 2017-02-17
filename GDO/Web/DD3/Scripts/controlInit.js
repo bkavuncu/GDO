5b66184c7d8fad99b4db787e171da5a40320264c
shabai517\shabai517 <shabai517@163.com>
2017-02-18 00:10:29 +0800

    Controlcshtml has been organized totally: 1. create file "controlInit.js" 2. create file "controlTestBench.js" 3. move the js code in "Control.cshtml" to "controlInit.js" and  "controlTestBench.js". 4. create file "control.css" 5. move the css code in "Control.cshtml" to  "control.css" 6. All the testings have been finished. All the apps works fine (except the bad ones in the orginial d3v4 branch, we should do some debugs for these bad apps later)
    Currently, this version is ready to merge. Xiaoping can download this code and add his content as we discussed before.

---
+++ GDO/Web/DD3/Scripts/controlInit.js
@@ -0,0 +1,135 @@
+﻿var instanceId;
+
+
+d3.selection.prototype.moveToFront = function () {
+    return this.each(function () {
+        this.parentNode.appendChild(this);
+    });
+};
+
+d3.selection.prototype.moveToBack = function () {
+    return this.each(function () {
+        var firstChild = this.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.insertBefore(this, firstChild);
+        }
+    });
+};
+
+
+
+var control = {
+    gdo: parent.gdo,
+    gdoApp: parent.gdo.net.app["DD3"].initControl,
+    server: parent.gdo.net.app.DD3.server,
+    dataset:null,
+    eventLog:[],
+    confId:null,
+    numClients:null,
+    test_bench: {},
+    init: function(callback) {
+        //BAI: not sure if the following code is useful or not
+        this.gdo.controlId = parseInt(this.getUrlVar("controlId"));
+        return this.gdoApp(callback);
+    },
+    getUrlVar: function (variable) {
+        /// <summary>
+        /// Gets the URL variable.
+        /// </summary>
+        /// <param name="variable">The variable.</param>
+        /// <returns></returns>
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (pair[0] === variable) {
+                return pair[1];
+            }
+        }
+        query = window.frames['control_frame_content'].location.search.substring(1);
+        vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (pair[0] === variable) {
+                return pair[1];
+            }
+        }
+        return false;
+    },
+    callback: function(message) {
+        control.gdo.consoleOut(".DD3", 4, "Callback Message: " + message);
+        //BAI: change num to string
+        control.confId = message.configurationId+'';
+        control.numClients = message.numClient;
+        if (message.state == 1) {
+            control.test_bench[control.confId] && control.test_bench[control.confId]();
+            control.test_bench[control.confId] = null;
+            $("#wait").css("display", "none");
+            //BAI:change the num to string
+            if (control.confId === '6' || control.confId === '7' || control.confId === '8' || control.confId === '9') {  // all of these are Shangai LinkLoad, and should share the control menu
+                $("#control_6").css("display", "");
+            }else {
+                $("#control_" + control.confId).css("display", "");
+            }
+        }
+    }
+};
+
+var peer = {
+    // == FPS ==
+    peerObject: { host: "146.169.32.109", port: 55555 },
+
+    init: function () {
+        //{ host: "localhost", port: 55555 };
+        //{ host: "146.169.32.109", port: 55555 }
+        var con_peer = new Peer("idofthecontrollerfordd3", this.peerObject);
+        con_peer.on('open', function(conn) {
+            control.gdo.consoleOut()(".DD3", 4, "The peer client of the controller is opened");
+        });
+        con_peer.on('connection', function(conn) {
+            control.eventLog.push([new Date().getTime(), "Connecting with  "+ conn.label]);
+            //BAI: "con_peer" is the controller peer connection object
+            conn.on('data', function (data) {
+                var raw = data.split(';');
+                var row = raw[0];
+                var col = raw[1];
+                var nbr = raw[2];
+                var nbc = raw[3];
+                var nbdc = raw[4];
+                var ts = raw[5];
+                var fps = raw[6];
+                var std = raw[7];
+                control.dataset.push([row, col, nbr, nbc, nbdc, ts, fps, std]);
+                //gdo.consoleOut(".DD3", 4, dataset);
+                //updateFPSChart();
+            });  
+        });
+    }
+}
+
+
+$(document).ready(function() {
+    controlTestBench.init(control.server, control.dataset, control.numClients, control.eventLog);
+    control.test_bench = controlTestBench.test_bench;
+
+    if (!control.gdoApp) {
+        $("#wait").css("display", "none");
+        //BAI: change num to string
+        if (control.confId === '6' || control.confId === '7' || control.confId === '8' || control.confId === '9') { // all of these are Shangai LinkLoad, and should share the control menu
+            $("#control_6").css("display", "");
+        } else {
+            $("#control_" + control.confId).css("display", "");
+        }
+    }
+
+    peer.init();
+
+    if (!control.gdoApp) {
+        $("#wait").css("display", "none");
+        $("#error").css("display", "");
+        return;
+    }
+
+    instanceId = control.init(control.callback);
+
+});
