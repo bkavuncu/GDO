160b0bd18a6435de299e7d44675b1eb08d0c7116
shabai517\shabai517 <shabai517@163.com>
2017-02-18 01:55:33 +0800

    basic structure of network interface

---
+++ GDO.Apps.DD3/Scripts/DD3/gdo.apps.dd3.net.js
@@ -0,0 +1,47 @@
+﻿//template for xiaoping
+
+var dd3Net = {
+    
+    peer: {
+        configPeer: {
+            host:,
+            port:,
+            id: null,
+            peers: [],
+            connections: [],
+            buffers: []
+        },
+        configCentral: {
+        
+        },
+
+        init: function(type) {
+            switch (type) {
+                case 'peer':
+
+                    break;
+                case 'central':
+                    break;
+            }
+        },
+        //Add data to database
+        connect: function (collection, data) { },
+        //Delete data in the database
+        flush: function (collection, data) { },
+        //Update new data to the database
+        send: function (collection, query, data) { },
+        // find a data from database.
+        find: function (collection, query) { }
+
+
+
+    },
+
+    signalr: {},
+    nodejs: {
+        
+    }
+};
+
+
+
