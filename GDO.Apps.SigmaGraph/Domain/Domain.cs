4d56c0f990505c73dde9162623a52f62a91cf29f
Timothy Truong Jr <ttruong@mit.edu>
2017-07-05 19:44:09 +0100

    Implemented IsWithin for GraphNode and GraphLink

--- GDO.Apps.SigmaGraph/Domain/Domain.cs
+++ GDO.Apps.SigmaGraph/Domain/Domain.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Concurrent;
+﻿using GDO.Apps.SigmaGraph.QuadTree;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using GDO.Apps.SigmaGraph.QuadTree;
 
 namespace GDO.Apps.SigmaGraph.Domain
 {
@@ -42,6 +42,13 @@ namespace GDO.Apps.SigmaGraph.Domain
     }
 
     public abstract class GraphObject : IQuadable<double> {
+        /// <summary>
+        /// Checks if this graph object is contained in the region
+        /// represented by the quadtree node.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="q"></param>
+        /// <returns></returns>
         public abstract bool IsWithin<T>(QuadTreeNode<T> q) where T : IQuadable<double>;
     }
 
@@ -56,6 +63,22 @@ namespace GDO.Apps.SigmaGraph.Domain
         public Dictionary<string, string> Attrs { get; set; }
         public int NumLinks { get; set; }
         public List<string> Adj { get; set; }  // adj = list of connectedNodes
+
+        public override bool IsWithin<T>(QuadTreeNode<T> quadTreeNode)
+        {
+            double xCentroid = quadTreeNode.centroid.xCentroid;
+            double yCentroid = quadTreeNode.centroid.yCentroid;
+            double xWidth = quadTreeNode.centroid.xWidth;
+            double yWidth = quadTreeNode.centroid.yWidth;
+
+            double minX = xCentroid - xWidth;
+            double maxX = xCentroid + xWidth;
+            double minY = yCentroid - yWidth;
+            double maxY = yCentroid + yWidth;
+            // todo check boundary conditions open or closed
+            return Pos.X >= minX && Pos.X <= maxX 
+                && Pos.Y >= minY && Pos.Y <= maxY;
+        }
     }
 
     public class GraphLink : GraphObject {
@@ -68,6 +91,71 @@ namespace GDO.Apps.SigmaGraph.Domain
         public int G { get; set; }
         public int B { get; set; }
         public Dictionary<string, string> Attrs { get; set; }
+        private const double Epsilon = 1E-6;
+
+        public override bool IsWithin<T>(QuadTreeNode<T> quadTreeNode)
+        {
+            double xCentroid = quadTreeNode.centroid.xCentroid;
+            double yCentroid = quadTreeNode.centroid.yCentroid;
+            double xWidth = quadTreeNode.centroid.xWidth;
+            double yWidth = quadTreeNode.centroid.yWidth;
+
+            double minX = xCentroid - xWidth;
+            double maxX = xCentroid + xWidth;
+            double minY = yCentroid - yWidth;
+            double maxY = yCentroid + yWidth;
+
+            bool quadContainsWholeLink = StartPos.X >= minX && EndPos.X >= minX
+                                      && StartPos.X <= maxX && EndPos.X <= maxX
+                                      && StartPos.Y >= minY && EndPos.Y >= minY
+                                      && StartPos.Y <= maxY && EndPos.Y <= maxY;
+
+            return quadContainsWholeLink
+                   || IntersectsHorizontalSegment(maxY, minX, maxX)
+                   || IntersectsHorizontalSegment(minY, minX, maxX)
+                   || IntersectsVerticalSegment(minX, minY, maxY)
+                   || IntersectsVerticalSegment(maxX, minY, maxY);
+        }
+
+        /// <summary>
+        /// Determines whether or not this link intersects the horizontal
+        /// line segment between (y, minX) and (y, maxX).
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="minX"></param>
+        /// <param name="maxX"></param>
+        /// <returns></returns> true if they intersect and false otherwise
+        private bool IntersectsHorizontalSegment(double y, double minX, double maxX)
+        {
+            if (EndPos.X - StartPos.X <= Epsilon)
+            {
+                return StartPos.X >= minX && StartPos.X <= maxX;
+            }
+            // The lines intersect at (x = 1/m (y-y_o) + x_0, y = y)
+            double slope = (EndPos.Y - StartPos.Y) / (EndPos.X - StartPos.X);
+            double xIntersect = 1 / slope * (y - StartPos.Y) + StartPos.X;
+            return xIntersect >= minX && xIntersect <= maxX;
+        }
+
+        /// <summary>
+        /// Determines whether or not this link intersects the vertical
+        /// line segment between (x, minY) and (x, maxY).
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="minY"></param>
+        /// <param name="maxY"></param>
+        /// <returns></returns> true if they intersect or if and false otherwise
+        private bool IntersectsVerticalSegment(double x, double minY, double maxY)
+        {
+            if (EndPos.X - StartPos.X <= Epsilon)
+            {
+                return StartPos.X - x <= Epsilon;
+            }
+            // The lines intersect at (x = x, y = m(x-x_0) + y_0)
+            double slope = (EndPos.Y - StartPos.Y) / (EndPos.X - StartPos.X);
+            double yIntersect = slope * (x - StartPos.X) + StartPos.Y;
+            return yIntersect >= minY && yIntersect <= maxY;
+        }
 
         public override string ToString() {
             return "Link = S:"+Source+" T:"+Target+ " | "+ StartPos+" to "+EndPos;
