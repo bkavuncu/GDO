b2a900539f0f81e8b1c696ac20da63d946dfd7b2
davidbirch <david.birch@imperial.ac.uk>
2017-07-10 22:00:09 +0100

    avoiding double fix

--- GDO.Apps.SigmaGraph/Domain/Domain.cs
+++ GDO.Apps.SigmaGraph/Domain/Domain.cs
@@ -72,10 +72,10 @@ namespace GDO.Apps.SigmaGraph.Domain
             double xWidth = quadTreeNode.centroid.xWidth/2.0;
             double yWidth = quadTreeNode.centroid.yWidth/2.0;
 
-            double minX = xCentroid - xWidth/2;
-            double maxX = xCentroid + xWidth/2;
-            double minY = yCentroid - yWidth/2;
-            double maxY = yCentroid + yWidth/2;
+            double minX = xCentroid - xWidth;
+            double maxX = xCentroid + xWidth;
+            double minY = yCentroid - yWidth;
+            double maxY = yCentroid + yWidth;
             return Pos.X >= minX && Pos.X <= maxX 
                 && Pos.Y >= minY && Pos.Y <= maxY;
         }
@@ -100,10 +100,10 @@ namespace GDO.Apps.SigmaGraph.Domain
             double xWidth = quadTreeNode.centroid.xWidth/2.0;
             double yWidth = quadTreeNode.centroid.yWidth/2.0;
 
-            double minX = xCentroid - xWidth/2;
-            double maxX = xCentroid + xWidth/2;
-            double minY = yCentroid - yWidth/2;
-            double maxY = yCentroid + yWidth/2;
+            double minX = xCentroid - xWidth;
+            double maxX = xCentroid + xWidth;
+            double minY = yCentroid - yWidth;
+            double maxY = yCentroid + yWidth;
 
             bool quadContainsWholeLink = StartPos.X >= minX && EndPos.X >= minX
                                       && StartPos.X <= maxX && EndPos.X <= maxX
