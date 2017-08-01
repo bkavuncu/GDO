d4b2cd42d13c5ec9d0045110fafd972475c708d6
Timothy Truong Jr <ttruong@mit.edu>
2017-08-01 11:02:53 +0100

    Include missing partial graph image model and controller
    This is the first commit for which partial graph image uploading works

---
+++ GDO.Apps.SigmaGraph/HTTPServer/PartialGraphImagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GDO.Apps.SigmaGraph.HTTPServer
+{
+    public class PartialGraphImagesController : ApiController
+    {
+        [Route("Web/SigmaGraph/images")]
+        public void Post([FromBody]ImageModel model)
+        {
+            string path = "Web/SigmaGraph/images/" + model.Name;
+            File.WriteAllText(path, model.Image);
+        }
+    }
+}
