942843b9ea64c271bebb965c8dab9f2f21d917ec
davidbirch <david.birch@imperial.ac.uk>
2017-10-31 15:07:45 +0000

    interfaces for posted content

--- GDO/Core/Apps/IAppInstance.cs
+++ GDO/Core/Apps/IAppInstance.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.IO;
+using Newtonsoft.Json;
 
 namespace GDO.Core.Apps
 {
@@ -18,5 +19,28 @@ namespace GDO.Core.Apps
 
         //IHubContext HubContext { get; set; }
         void Init();
+    }
+
+    /// <summary>
+    /// an interface which apps can implement to say that they accept loading content from a URI
+    /// </summary>
+    public interface IAppSupportsLoadingURI {
+        /// <summary>
+        /// Loads from URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="updateOnLoad">if set to <c>true</c> [update on load].</param>
+        void LoadFromURI(string uri, bool updateOnLoad = false);
+    }
+
+    /// <summary>
+    /// an interface which apps can implement to say that they accept loading content via POST
+    /// </summary>
+    public interface IAppSupportsPostedContent {
+        /// <summary>
+        /// Loads from stream. (what is in the stream is app specific)
+        /// </summary>
+        /// <param name="data">The data.</param>
+        void LoadFromStream(Stream data);
     }
 }
