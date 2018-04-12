52f80b05ca80e8847a548298336eb33d972a8fba
Senaka Fernando <senakafdo@gmail.com>
2018-04-12 13:06:36 +0100

    Fixing bug in node size calculation going below.

--- GDO.Apps.SigmaGraph/Scripts/SigmaGraph/gdo.apps.SigmaGraph.render.js
+++ GDO.Apps.SigmaGraph/Scripts/SigmaGraph/gdo.apps.SigmaGraph.render.js
@@ -461,7 +461,9 @@ function handleFileGraphObjects(fileGraphObjects) {
             node.color = "#89f";
         }
         // What should the max be? 5? 12?
-        node.size = Math.min(12, node.size) || 3;
+        // node.size = Math.min(12, node.size) || 3;
+        // Fixed the above line since it did not look correct - Senaka
+        node.size = Math.max(3, Math.min(12, node.size));
         try {
             gdo.sigmaInstance.graph.addNode(node);
         } catch (err) {
